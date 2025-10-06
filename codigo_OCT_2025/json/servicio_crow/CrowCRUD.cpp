363043a73d4d3c12eada4de300515738a4c41a41
aldebarran22 <antonioespinherranz@hotmail.com>
2025-10-06 14:01:47 +0200

    33

--- codigo_OCT_2025/json/servicio_crow/CrowCRUD.cpp
+++ codigo_OCT_2025/json/servicio_crow/CrowCRUD.cpp
@@ -41,16 +41,59 @@ void CrowCRUD::run() {
 		// Chequear si hemos encontrado la clave:
 		if (!n) {
 			// Si no lo encontramos:
-			crow::response(404, "Usuario " + std::to_string(id) + " no encontrado");
+			return crow::response(404, "Usuario " + std::to_string(id) + " no encontrado");
 		}
 
-		// Los hemos encontrado:
-		crow::json:wvalue res;
+		// Lo hemos encontrado devolvemos nombre y email
+		crow::json::wvalue res;
+		res["nombre"] = usuarios[id]["nombre"].s();
+		res["email"] = usuarios[id]["email"].s();
+
+		return crow::response(res);
 
 	});
 	
-	
+	CROW_ROUTE(app, "/usuarios/<int>").methods(crow::HTTPMethod::Delete)([this](int id) {
+		std::lock_guard<std::mutex> lock(this->mtx);
 
+		if (usuarios.erase(id) == 0)
+			return crow::response(404, "Usuario " + std::to_string(id) + " no encontrado");
+
+		// Devolvemos solo el código 204, no tiene contenido, pero si exito
+		crow::response res;
+		res.code = 204;
+		return res;
+	});
+
+	CROW_ROUTE(app, "/usuarios").methods(crow::HTTPMethod::GET)([this]() {
+		std::lock_guard<std::mutex> lock(this->mtx);
+
+		// Montar una lista con todos los usuarios:
+		crow::json::wvalue lista = crow::json::wvalue::list();
+		int i = 0;
+
+		for (const auto& [id, usuario] : usuarios) {
+			crow::json::wvalue item;
+			item["id"] = id;
+
+			if (usuario.has("nombre"))
+				item["nombre"] = usuario["nombre"].s();
+			else
+				item["nombre"] = "";
+
+			if (usuario.has("email"))
+				item["email"] = usuario["email"].s();
+			else
+				item["email"] = "";
+
+			lista[i++] = std::move(item);
+		}
+
+		crow::json::wvalue res;
+		res["usuarios"] = std::move(lista);
+		return crow::response(res);
+
+	});
 }
 CrowCRUD::~CrowCRUD() {
 
