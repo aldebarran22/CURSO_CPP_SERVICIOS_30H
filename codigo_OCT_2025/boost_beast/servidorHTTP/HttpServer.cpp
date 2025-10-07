7b169d4e174bfed56f612a9063beab140d1d6f8c
aldebarran22 <antonioespinherranz@hotmail.com>
2025-10-07 09:57:38 +0200

    87

--- codigo_OCT_2025/boost_beast/servidorHTTP/HttpServer.cpp
+++ codigo_OCT_2025/boost_beast/servidorHTTP/HttpServer.cpp
@@ -1,4 +1,5 @@
 #include "HttpServer.h"
+#include "boost/exception/diagnostic_information.hpp"
 
 
 HttpServer::HttpServer(net::io_context& ioc, unsigned short port):ioc_(ioc), acceptor_(ioc, tcp::endpoint(tcp::v4(), port)){}
@@ -18,26 +19,32 @@ void HttpServer::handle_request(tcp::socket& socket) {
 	http::request<http::string_body> request;
 	std::string mensaje = "";
 
-	// Leer la peticion:
-	http::read(socket, buffer, request);
+	try {
+		// Leer la peticion:
+		http::read(socket, buffer, request);
 
-	// Extraer el mensaje y la queryString con el parametro:
-	std::string target = request.target();
-	std::cout << "Peticion: " << target << std::endl;
+		// Extraer el mensaje y la queryString con el parametro:
+		std::string target = request.target();
+		std::cout << "Peticion: " << target << std::endl;
 
-	// Buscar el parametro de la url:
-	auto pos = target.find("?msg=");
+		// Buscar el parametro de la url:
+		auto pos = target.find("?msg=");
 
-	if (pos != std::string::npos) {
-		mensaje = target.substr(pos + 5);
+		if (pos != std::string::npos) {
+			mensaje = target.substr(pos + 5);
+		}
+
+		// Montar la respuesta al cliente:
+		http::response<http::string_body> response{ http::status::ok, request.version() };
+		response.set(http::field::server, "Beast/1.0");
+		response.set(http::field::content_type, "text/plain");
+		response.body() = "Respuesta del servidor. Mensaje: " + mensaje;
+
+		// Escribir la respuesta:
+		http::write(socket, response);
+
 	}
-
-	// Montar la respuesta al cliente:
-	http::response<http::string_body> response{ http::status::ok, request.version() };
-	response.set(http::field::server, "Beast/1.0");
-	response.set(http::field::content_type, "text/plain");
-	response.body() = "Respuesta del servidor. Mensaje: " + mensaje;
-
-	// Escribir la respuesta:
-	http::write(socket, response);
+	catch (const std::exception& e) {
+		std::cerr << boost::diagnostic_information(e) << std::endl;
+	}
 }
