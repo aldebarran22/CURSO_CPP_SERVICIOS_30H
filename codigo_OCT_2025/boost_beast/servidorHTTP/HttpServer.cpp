6c049a65ad2a2934ac0e63847c14565272e024d9
aldebarran22 <antonioespinherranz@hotmail.com>
2025-10-07 09:42:55 +0200

    87

--- codigo_OCT_2025/boost_beast/servidorHTTP/HttpServer.cpp
+++ codigo_OCT_2025/boost_beast/servidorHTTP/HttpServer.cpp
@@ -14,5 +14,26 @@ void HttpServer::run() {
 }
 
 void HttpServer::handle_request(tcp::socket& socket) {
+	beast::flat_buffer buffer;
+	http::request<http::string_body> request;
+	std::string mensaje = "";
 
+	// Leer la peticion:
+	http::read(socket, buffer, request);
+
+	// Extraer el mensaje y la queryString con el parametro:
+	std::string target = request.target();
+	std::cout << "Peticion: " << target << std::endl;
+
+	// Buscar el parametro de la url:
+
+
+	// Montar la respuesta al cliente:
+	http::response<http::string_body> response{ http::status::ok, request.version() };
+	response.set(http::field::server, "Beast/1.0");
+	response.set(http::field::content_type, "text/plain");
+	response.body() = "Respuesta del servidor";
+
+	// Escribir la respuesta:
+	http::write(socket, response);
 }
