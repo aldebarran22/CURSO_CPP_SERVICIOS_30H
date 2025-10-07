eab051343621df36a99fc542bd2158820a7a7006
aldebarran22 <antonioespinherranz@hotmail.com>
2025-10-07 09:41:55 +0200

    87

--- codigo_OCT_2025/boost_beast/servidorHTTP/HttpServer.cpp
+++ codigo_OCT_2025/boost_beast/servidorHTTP/HttpServer.cpp
@@ -1,12 +1,16 @@
 #include "HttpServer.h"
 
 
-HttpServer::HttpServer(net::io_context& ioc, unsigned short port) {
-
-}
+HttpServer::HttpServer(net::io_context& ioc, unsigned short port):ioc_(ioc), acceptor_(ioc, tcp::endpoint(tcp::v4(), port)){}
 
 void HttpServer::run() {
+	std::cout << "Servidor HTTP " << acceptor_.local_endpoint().port() << std::endl;
 
+	for (;;) {
+		tcp::socket socket(ioc_);
+		acceptor_.accept(socket);
+		handle_request(socket);
+	}
 }
 
 void HttpServer::handle_request(tcp::socket& socket) {
