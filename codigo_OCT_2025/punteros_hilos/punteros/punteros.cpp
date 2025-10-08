13c52ba67a54f339a994ea25da110a39abf69352
aldebarran22 <antonioespinherranz@hotmail.com>
2025-10-08 14:36:11 +0200

    87

--- codigo_OCT_2025/punteros_hilos/punteros/punteros.cpp
+++ codigo_OCT_2025/punteros_hilos/punteros/punteros.cpp
@@ -2,7 +2,26 @@
 //
 
 #include <iostream>
+#include <cstdlib>
 #include "Vector.h"
+
+void testC() {
+    int n = 10;
+    int* ptr= nullptr;
+    int* ptr2 = nullptr;
+
+    ptr = (int*)std::malloc(sizeof(int) * n); // Bloque completo en bytes
+    ptr2 = (int*)std::calloc(n, sizeof(int)); // numero de elementos * ocupan, inicializa a cero los elementos
+
+    // Realloc, ampliar un bloque de memoria:
+    // Ampliar m elementos:
+    int m = 5;
+
+    ptr = (int*)std::realloc(ptr, (static_cast<unsigned long long>(n) + m) * sizeof(int)); // puntero original y tamaño total final.
+
+    free(ptr);
+    free(ptr2);
+}
 
 void testVector() {
     Vector v1, v3;
