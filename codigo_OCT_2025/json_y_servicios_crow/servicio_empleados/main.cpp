3b792d2e9977fc492ad402415250bc8f86114263
aldebarran22 <antonioespinherranz@hotmail.com>
2025-10-08 09:28:15 +0200

    87

--- codigo_OCT_2025/json_y_servicios_crow/servicio_empleados/main.cpp
+++ codigo_OCT_2025/json_y_servicios_crow/servicio_empleados/main.cpp
@@ -2,6 +2,29 @@
 //
 
 #include <iostream>
+#include <soci/soci.h>
+#include <soci/mysql/soci-mysql.h>
+
+#include "Empleado.h"
+#include "EmpleadoRepositorio.h"
+
+void testRepositorio() {
+    soci::session sql(soci::mysql, "db=empresa3 user=antonio password=antonio host=127.0.0.1 port=3307");
+    EmpleadoRepositorio repo(sql);
+
+    // Recuperar un empleado:
+    auto emp = repo.read(1);
+    if (emp) {
+        std::cout << emp->nombre << " " << emp->cargo << std::endl;
+    }
+    else {
+        std::cerr << "No se encontro el empleado ... " << std::endl;
+    }
+
+    for (const auto& emp : repo.select()) {
+        std::cout << emp.id << " " << emp.nombre << " " << emp.cargo << std::endl;
+    }
+}
 
 int main()
 {
