c8c8d58ea84e9f74e8fedb6961c7b2ef7295857c
aldebarran22 <antonioespinherranz@hotmail.com>
2025-10-08 12:30:31 +0200

    87

---
+++ codigo_OCT_2025/boost_beast/multicliente/multicliente.cpp
@@ -0,0 +1,63 @@
+#include <boost/beast/core.hpp>
+#include <boost/beast/http.hpp>
+#include <boost/beast/version.hpp>
+#include <boost/asio.hpp>
+
+#include <iostream>
+#include <string>
+#include <memory>
+
+namespace beast = boost::beast;
+namespace http = beast::http;
+namespace asio = boost::asio;
+using tcp = asio::ip::tcp;
+
+class session : public std::enable_shared_from_this<session> {
+
+	// Atributos de la sesion:
+	tcp::socket socket_;
+	beast::flat_buffer buffer_;
+	http::request<http::string_body> req_;
+	http::response<http::string_body> res_;
+
+public:
+	explicit session(tcp::socket socket):socket_(std::move(socket)){} // No hacemos copia del socket!
+
+	void start() { read_request(); }
+
+private:
+	void read_request() {
+		// Obtener un std::shared_ptr que apunta a la clase actual:
+		auto self = shared_from_this();
+
+		// Hacer una lectura asincrona:
+		http::async_read(socket_, buffer_, req_,
+			[self](beast::error_code ec, std::size_t) {
+				if (!ec) self->handler_request();
+			});
+	}
+
+	void handler_request() {
+		// Analizar la peticion, ... operaciones ...
+		
+		// Montar la respuesta al cliente:
+		// La respuesta se copia, misma version del protocolo que la peticion:
+		res_.version(req_.version());
+		res_.keep_alive(req_.keep_alive());
+		res_.result(http::status::ok); // Codigo 200
+
+		// Configurar las cabeceras de la respuesta:
+
+	}
+
+	void write_response() {
+
+	}
+
+};
+
+int main()
+{
+   
+}
+
