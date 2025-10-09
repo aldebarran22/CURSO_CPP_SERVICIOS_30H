dd285f964284a2f8ac9bd3dbf05cf664ecbdc7ce
aldebarran22 <antonioespinherranz@hotmail.com>
2025-10-09 09:27:38 +0200

    87

--- codigo_OCT_2025/comunicaciones_zmq/peticion_respuesta_cliente/peticion_respuesta_cliente.cpp
+++ codigo_OCT_2025/comunicaciones_zmq/peticion_respuesta_cliente/peticion_respuesta_cliente.cpp
@@ -5,18 +5,24 @@
 
 int main()
 {
-	// El cliente se conecta al mismo puerto:
-	zmq::context_t contexto(1);
+	try {
+		// El cliente se conecta al mismo puerto:
+		zmq::context_t contexto(1);
 
-	zmq::socket_t socket(contexto, zmq::socket_type::req);
-	socket.connect("tcp:://localhost:5555");
+		zmq::socket_t socket(contexto, zmq::socket_type::req);
+		socket.connect("tcp://localhost:5555");
 
-	// Enviar un mensaje al servidor:
-	socket.send(zmq::buffer("mensaje del cliente"), zmq::send_flags::none);
+		// Enviar un mensaje al servidor:
+		socket.send(zmq::buffer("mensaje del cliente"), zmq::send_flags::none);
 
-	zmq::message_t respuesta;
-	socket.recv(respuesta, zmq::recv_flags::none);
-	std::cout << "Respuesta del servidor: " << respuesta.to_string() << std::endl;
+		zmq::message_t respuesta;
+		socket.recv(respuesta, zmq::recv_flags::none);
+		std::cout << "Respuesta del servidor: " << respuesta.to_string() << std::endl;
+
+	}
+	catch (std::exception& e) {
+		std::cerr << "Error " << e.what() << std::endl;
+	}
 
 	return 0;
 }
