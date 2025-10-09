31f42b3dc7abd6cbfa40f63a3480bf75dd48e270
aldebarran22 <antonioespinherranz@hotmail.com>
2025-10-09 14:47:06 +0200

    87

---
+++ codigo_OCT_2025/comunicaciones_web_sockets/web_sockets/ws_servidor/ws_servidor.cpp
@@ -0,0 +1,44 @@
+// ws_servidor.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
+//
+
+#include <iostream>
+#include <thread>
+#include <boost/beast/core.hpp>
+#include <boost/beast/websocket.hpp>
+#include <boost/asio.hpp>
+
+namespace beast = boost::beast;
+namespace websocket = beast::websocket;
+namespace net = boost::asio;
+using tcp = net::ip::tcp;
+
+void do_sesion(tcp::socket socket) {
+
+}
+
+int main()
+{
+	net::io_context ioc;
+	tcp::acceptor acceptor(ioc, tcp::endpoint(tcp::v4(), 80));
+
+	try {
+
+		std::cout << "Servidor ok en el puerto 80 " << std::endl;
+
+		for (;;) {
+			// Definir un socket (1 por cada cliente)
+			tcp::socket socket(ioc);
+
+			std::cout << "Esperando clientes ...";
+			acceptor.accept(socket);
+
+			// Creamos un hilo para atender a cada cliente:
+			std::thread{ do_sesion, std::move(socket) }.detach(); // se desconecta del hilo main
+		}
+
+	}
+	catch (const std::exception& e) {
+		std::cerr << "ERROR: " << e.what() << std::endl;
+	}
+   
+}
