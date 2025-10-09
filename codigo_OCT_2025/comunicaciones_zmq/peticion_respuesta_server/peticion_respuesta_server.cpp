a21d007aba611e285f234f8b003c78e3464cff47
aldebarran22 <antonioespinherranz@hotmail.com>
2025-10-09 09:16:00 +0200

    87

---
+++ codigo_OCT_2025/comunicaciones_zmq/peticion_respuesta_server/peticion_respuesta_server.cpp
@@ -0,0 +1,29 @@
+// peticion_respuesta_server.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
+//
+
+#include <iostream>
+#include <zmq.hpp>
+
+int main()
+{
+	zmq::context_t contexto(1);
+	zmq::socket_t socket(contexto, zmq::socket_type::rep);
+
+	// Hacer el bind al puerto:
+	socket.bind("tcp://*:5555");
+
+	std::cout << "Servidor zmq en el puerto 5555" << std::endl;
+	while (true) {
+		zmq::message_t request;
+
+		// Esperamos el mensaje del cliente:
+		socket.recv(request, zmq::recv_flags::none);
+		std::cout << "Recibido: " << request.to_string() << std::endl;
+
+		// Enviamos el mensaje al cliente:
+		std::string respuesta = "mensaje del server";
+		socket.send(zmq::buffer(respuesta), zmq::send_flags::none);
+	}
+
+	return 0;
+}
