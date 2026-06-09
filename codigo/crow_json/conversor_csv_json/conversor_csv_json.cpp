46ce3945d159566e97ad8192dfb065a36cb13234
aldebarran22 <antonioespinherranz@hotmail.com>
2026-06-09 13:38:07 +0200

    12

--- codigo/crow_json/conversor_csv_json/conversor_csv_json.cpp
+++ codigo/crow_json/conversor_csv_json/conversor_csv_json.cpp
@@ -2,8 +2,50 @@
 //
 
 #include <iostream>
+#include <fstream>
+#include <sstream>
+#include <vector>
+#include <nlohmann/json.hpp>
+
+#include "Pedido.h"
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    std::string pathIn = "..\\..\\..\\practicas\\ficheros\\pedidos_final.csv";
+    std::string pathOut = "..\\..\\..\\practicas\\ficheros\\out\\pedidos.json";
+
+    std::ifstream fin(pathIn);
+    std::ofstream fout(pathOut);
+
+    std::string linea;
+    std::vector<std::string> campos;
+    bool cabs = true;
+
+
+    if (!fin) {
+        std::cerr << "No se ha encontrado el fichero: " << pathIn << std::endl;
+        return;
+    }
+
+    while (std::getline(fin, linea)) {
+        std::cout << linea << std::endl;
+
+        if (cabs) {
+            cabs = false;
+
+        }
+        else {            
+            Pedido p(linea);
+
+        }
+    }
+    fin.close();
+
+    // Convertir y tabular;
+
+    // Imprimir a consola:
+
+
+    fout.close();
+    return 0;
 }
