6e8e17f9684e362b85e41c01239a397e936bc4d1
aldebarran22 <antonioespinherranz@hotmail.com>
2026-06-09 10:28:45 +0200

    87

--- codigo/crow_json/crow_basico/crow_basico.cpp
+++ codigo/crow_json/crow_basico/crow_basico.cpp
@@ -7,9 +7,23 @@
 void testCrow() {
     crow::SimpleApp app;
 
-    // Configuracion de rutas:
+    // CROW_ROUTE(app, url/<tipo1>/<tipo2>).methods( ...)([lista_captura](param1, param2){ cuerpo de la funcion })
+
+    // Configuracion de rutas: si no se indica el metodo por defecto es GET
     CROW_ROUTE(app, "/")([]() {
         return "Hello World";
+        });
+
+    CROW_ROUTE(app, "/info")([]() {
+        // Devolver una respuesta en json:
+        crow::json::wvalue resp;
+
+        resp["mensaje"] = "todo ok";
+        resp["codigo"] = 200;
+
+        // Devolver la respuesta al cliente:
+        return crow::response{ resp };
+
         });
 
     // Configurar el servidor en el puerto 18000 y poner en marcha!
