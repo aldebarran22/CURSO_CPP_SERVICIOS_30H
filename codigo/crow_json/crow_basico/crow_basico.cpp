078521c180a804871073c4ea64a69f3673786316
aldebarran22 <antonioespinherranz@hotmail.com>
2026-06-09 10:38:26 +0200

    87

--- codigo/crow_json/crow_basico/crow_basico.cpp
+++ codigo/crow_json/crow_basico/crow_basico.cpp
@@ -2,6 +2,7 @@
 //
 
 #include <iostream>
+#include <thread>
 #include <crow.h>
 
 void testCrow() {
@@ -26,8 +27,28 @@ void testCrow() {
 
         });
 
+    // Ejemplo con parametros:
+    CROW_ROUTE(app, "/operacion/<int>/<int>").methods(crow::HTTPMethod::GET)([](const crow::request& req, int a, int b) {
+        // Devolver una respuesta en json:
+        crow::json::wvalue resp;
+
+        int suma = a + b;
+        resp["a"] = a;
+        resp["b"] = b;
+        resp["suma"] = a + b;
+
+        return crow::response{ resp };
+        });
+
     // Configurar el servidor en el puerto 18000 y poner en marcha!
-    app.port(18000).run();
+    app.port(18000);
+       
+    app.multithreaded();
+
+    // Elegir el numero de hilos:
+    app.concurrency(std::thread::hardware_concurrency());
+
+    app.run();
 }
 
 
