7f9b74b09e46bc095df9cb7d731a5814acdd4650
aldebarran22 <antonioespinherranz@hotmail.com>
2026-06-10 11:04:59 +0200

    87

--- codigo/web_sockets/ws_server/ws_server.cpp
+++ codigo/web_sockets/ws_server/ws_server.cpp
@@ -12,6 +12,39 @@ namespace websocket = beast::websocket;
 namespace net = boost::asio;
 using tcp = net::ip::tcp;
 
+void do_session(tcp::socket socket) {
+    // Definir un stream con un socket TCP: con el socket original que se conecto, NO hacemos copias
+
+    try {
+        websocket::stream<tcp::socket> ws(std::move(socket));
+        ws.accept();
+
+        for (;;) {
+            // Definir un buffer para L/E:
+            beast::flat_buffer buffer;
+
+            ws.read(buffer);
+            ws.text(ws.got_text());
+
+            // Devolver el mismo mensaje recibido:
+            ws.write(buffer.data());
+        }
+
+    } catch (const beast::system_error& se) {
+        if (se.code() != websocket::error::closed) {
+            std::cerr << "Error en sesion : " << se.what() << std::endl;
+        }
+        else {
+            std::cout << "Cliente desconectado" << std::endl;
+        }
+
+    }
+    catch (const std::exception& e) {
+        std::cerr << "Error inesperado " << e.what() << std::endl;
+    }
+
+}
+
 int main()
 {
     // Crear el contexto de comunicacion:
