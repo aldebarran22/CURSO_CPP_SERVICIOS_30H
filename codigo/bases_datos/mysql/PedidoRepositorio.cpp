9d00f14e43669d080c71ab92860a136a7f26f377
aldebarran22 <antonioespinherranz@hotmail.com>
2026-06-10 13:10:19 +0200

    12

--- codigo/bases_datos/mysql/PedidoRepositorio.cpp
+++ codigo/bases_datos/mysql/PedidoRepositorio.cpp
@@ -1,3 +1,5 @@
+#include <string>
+
 #include "PedidoRepositorio.h"
 
 PedidoRepositorio::PedidoRepositorio(soci::session& sql):sql_(sql)
@@ -6,7 +8,20 @@ PedidoRepositorio::PedidoRepositorio(soci::session& sql):sql_(sql)
 
 std::optional<Pedido> PedidoRepositorio::read(int id)
 {
-	return std::optional<Pedido>();
+	Pedido p;
+	soci::indicator ind;
+
+	std::string sql = std::string("select p.idpedido, p.idcliente, e.nombre as empleado, ") +
+		std::string("emp.nombre as empresa, p.importe, p.pais from pedidos p inner join empresasenvios emp ") +
+		std::string("on p.idempresaenvio = emp.id inner join empleados e on p.idempleado = e.id where idpedido = :id");
+	sql_ << sql, soci::use(id), soci::into(p.idpedido, ind), soci::into(p.cliente), soci::into(p.empresa),
+		soci::into(p.empleado), soci::into(p.importe), soci::into(p.pais);
+
+	if (ind == soci::i_null) {
+		return std::nullopt;
+	}
+
+	return p;
 }
 
 bool PedidoRepositorio::create(const Pedido& p)
