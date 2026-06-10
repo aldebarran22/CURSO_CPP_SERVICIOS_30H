655cee3da1d1f782ff854774de5d8765e323793a
aldebarran22 <antonioespinherranz@hotmail.com>
2026-06-10 13:57:54 +0200

    87

--- codigo/bases_datos/mysql/mysql.cpp
+++ codigo/bases_datos/mysql/mysql.cpp
@@ -7,9 +7,51 @@
 #include <soci/mysql/soci-mysql.h>
 
 #include "Pedido.h"
+#include "Empleado.h"
 #include <vector>
 
 #include "PedidoRepositorio.h"
+#include "EmpleadoRepositorio.h"
+
+
+void test2Repo() {
+    soci::session sql(soci::mysql, "db=empresa3 user=antonio password=antonio host=127.0.0.1 port=3307");
+
+    EmpleadoRepository repo(sql);
+
+    try {
+        // Eliminar
+        repo.eliminar(10);
+
+        // Insertar
+        repo.insertar({ 10, "Laura", "Directivo" });
+
+        // Obtener
+        int id = 10;
+        auto e = repo.recuperarEmpleado(id);
+        if (e) {
+            std::cout << e->nombre << " - " << e->cargo << std::endl;
+        }
+        else {
+            std::cout << "No se encuentra el empleado con id: " << id << std::endl;
+        }
+
+        // Listar
+        for (const auto& emp : repo.listarTodos()) {
+            std::cout << emp.id << ": " << emp.nombre << " (" << emp.cargo << ")\n";
+        }
+
+        // Actualizar
+        if (e) {
+            e->cargo = "Directora Creativa";
+            repo.actualizar(*e);
+        }
+
+    }
+    catch (std::exception& e) {
+        std::cout << "ERROR: " << e.what() << std::endl;
+    }
+}
 
 
 int main()
