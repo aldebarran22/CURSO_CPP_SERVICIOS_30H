f39bad0f9071aa95ab2969f74356cb9d018d8056
aldebarran22 <antonioespinherranz@hotmail.com>
2026-06-10 13:37:20 +0200

    87

--- codigo/bases_datos/mysql/mysql.cpp
+++ codigo/bases_datos/mysql/mysql.cpp
@@ -20,7 +20,7 @@ int main()
 		auto pedido = repo.read(id);
 
 		if (pedido) {
-			std::cout << pedido->idpedido << " " << pedido->cliente << std::endl;
+			std::cout << pedido->idpedido << " " << pedido->cliente << " " << pedido->importe << std::endl;
 		}
 		else {
 			std::cout << "No exite el pedido: " << id << std::endl;
