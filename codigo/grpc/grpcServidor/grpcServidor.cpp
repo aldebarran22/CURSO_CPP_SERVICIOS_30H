95f632c1af714b51c325fac2a877cbce73fc32f9
aldebarran22 <antonioespinherranz@hotmail.com>
2026-06-10 09:35:50 +0200

    87

--- codigo/grpc/grpcServidor/grpcServidor.cpp
+++ codigo/grpc/grpcServidor/grpcServidor.cpp
@@ -2,18 +2,41 @@
 //
 
 #include <iostream>
+#include <memory>
+#include <string>
+#include <grpcpp/grpcpp.h>
+#include "saludo.grpc.pb.h"
+
+using grpc::Server;
+using grpc::ServerBuilder;
+using grpc::ServerContext;
+using grpc::Status;
+
+using saludo::Saludo;
+using saludo::Solicitud;
+using saludo::Respuesta;
+
+
+class SaludoServiceImpl final : public Saludo::Service {
+    // Hereda de Service y sobrescribe el metodo virtual DiHola, aqui implementamos 
+    // la logica de lo que queremos hacer dentro del metodo rpc:
+    Status DiHola(ServerContext* context, const Solicitud* request, Respuesta* reply) override {
+        
+        // El nombre de la peticion
+        std::string nombre = request->nombre();
+
+        // Montar el mensaje de salida:
+        reply->set_mensaje("Hola " + nombre);
+
+        return Status::OK;
+    }
+
+};
+
+
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    
 }
 
-// Ejecutar programa: Ctrl + F5 o menú Depurar > Iniciar sin depurar
-// Depurar programa: F5 o menú Depurar > Iniciar depuración
-
-// Sugerencias para primeros pasos: 1. Use la ventana del Explorador de soluciones para agregar y administrar archivos
-//   2. Use la ventana de Team Explorer para conectar con el control de código fuente
-//   3. Use la ventana de salida para ver la salida de compilación y otros mensajes
-//   4. Use la ventana Lista de errores para ver los errores
-//   5. Vaya a Proyecto > Agregar nuevo elemento para crear nuevos archivos de código, o a Proyecto > Agregar elemento existente para agregar archivos de código existentes al proyecto
-//   6. En el futuro, para volver a abrir este proyecto, vaya a Archivo > Abrir > Proyecto y seleccione el archivo .sln
