4bbc0482ee6da8a05b84d6967a1a0c8558f75241
aldebarran22 <antonioespinherranz@hotmail.com>
2026-06-10 13:18:06 +0200

    87

--- codigo/bases_datos/mysql/mysql.cpp
+++ codigo/bases_datos/mysql/mysql.cpp
@@ -2,8 +2,32 @@
 //
 
 #include <iostream>
+#include <optional>
+#include <soci/soci.h>
+#include <soci/mysql/soci-mysql.h>
+
+#include "Pedido.h"
+#include "PedidoRepositorio.h"
+
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	try {
+		soci::session sql(soci::mysql, "db=empresa3 user=root password=antonio host=127.0.0.1 port=3307");
+		PedidoRepositorio repo(sql);
+
+		int id = 10250;
+		auto pedido = repo.read(id);
+
+		if (pedido) {
+			std::cout << pedido->idpedido << " " << pedido->importe << std::endl;
+		}
+		else {
+			std::cout << "No exite el pedido: " << id << std::endl;
+		}
+
+	}
+	catch (const std::exception& e) {
+		std::cerr << e.what() << std::endl;
+   }
 }
