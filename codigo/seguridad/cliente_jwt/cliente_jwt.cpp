72b9b64f0293dc10fe1a113321af2412f55b0ee5
aldebarran22 <antonioespinherranz@hotmail.com>
2026-06-11 12:37:11 +0200

    87

---
+++ codigo/seguridad/cliente_jwt/cliente_jwt.cpp
@@ -0,0 +1,12 @@
+// cliente_jwt.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
+//
+
+#include <iostream>
+#include <string>
+
+#include <cpr/cpr.h>
+
+int main()
+{
+    std::string datos = "{\"user\":\"admin\",\"pwd\" : \"1234\"}";
+}
