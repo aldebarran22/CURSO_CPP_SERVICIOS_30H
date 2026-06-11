4d3e1749e4430e81a652901e1a0cc344369db508
aldebarran22 <antonioespinherranz@hotmail.com>
2026-06-11 09:52:46 +0200

    87

--- codigo/crow_json/crow_bd_cache/PedidoService.cpp
+++ codigo/crow_json/crow_bd_cache/PedidoService.cpp
@@ -4,3 +4,25 @@ PedidoService::PedidoService(PedidoCache& cache, PedidoRepositorio& repositorio)
 {
 
 }
+
+std::optional<Pedido> PedidoService::read(int id)
+{
+	// 1) Buscar en la cache:
+	auto p = this->cache.getPedido(id);
+
+	if (!p) {
+		// Si no está en la cache:
+		p = this->repositorio.read(id);
+
+		if (p) {
+			// Si esta en el repositorio se guarda en la cache:
+			this->cache.savePedido(*p);
+		}
+	}
+
+	return p;
+}
+
+PedidoService::~PedidoService()
+{
+}
