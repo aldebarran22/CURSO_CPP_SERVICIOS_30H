ab89ac49ff3c94168711a9db4f4f2b5cbfc47a30
aldebarran22 <antonioespinherranz@hotmail.com>
2026-06-11 10:16:49 +0200

    87

--- codigo/crow_json/crow_bd_cache/PedidoCROW.cpp
+++ codigo/crow_json/crow_bd_cache/PedidoCROW.cpp
@@ -1,3 +1,6 @@
+#include <crow.h>
+#include <nlohmann/json.hpp>
+
 #include "PedidoCROW.h"
 
 PedidoCROW::PedidoCROW(PedidoService&):service(service)
@@ -6,22 +9,30 @@ PedidoCROW::PedidoCROW(PedidoService&):service(service)
 
 void PedidoCROW::run()
 {
-	/*
+	
 	// Configurar el servicio con las operaciones: GET / POST / PUT / DELETE
 	crow::SimpleApp app;
 
-	CROW_ROUTE(app, "/pedidos/<int>").methods(crow::HTTPMethod::GET)([](int id) {
+	CROW_ROUTE(app, "/pedidos/<int>").methods(crow::HTTPMethod::GET)([this](int id) {
 
-		
+		try {
+			// Verificar que existe:
+			auto pedido = this->service.read(id);
 
-		// El usuario existe hay que devolverlo:
-		crow::json::wvalue res;
-		res["id"] = id;
-		res["nombre"] = usuarios[id]["nombre"].s();
-		res["edad"] = usuarios[id]["edad"].i();
+			if (!pedido.has_value()) {
+				// No hemos encontrado el pedido (ni en la cache ni en mysql)
+				return crow::response(404, "Pedido con id: " + std::to_string(id) + " no existe");
+			}
 
-		return crow::response(res);
-		});
+			// El pedido existe hay que devolverlo:
+			json j = *pedido;
+			return crow::response(j.dump());
+
+		}
+		catch (const std::exception& e) {
+			return crow::response(500, std::string("Error en el servicio: ") + e.what());
+		}
+	});
 
 	// Get - GET /usuarios
 	CROW_ROUTE(app, "/usuarios").methods(crow::HTTPMethod::GET)([this]() {
