ab41acfff04d4f511fd69353d11e621d4839b7f6
aldebarran22 <antonioespinherranz@hotmail.com>
2026-06-11 11:29:47 +0200

    87

--- codigo/crow_json_completo_empleados/servicio_empleados_cache/main.cpp
+++ codigo/crow_json_completo_empleados/servicio_empleados_cache/main.cpp
@@ -12,7 +12,7 @@
 
 void arrancarServicio() {
     // Crear la session de soci:
-    soci::session sql(soci::mysql, "db=empresa3 user=antonio password=antonio host=127.0.0.1 port=3307");
+    soci::session sql(soci::mysql, "db=empresa3 user=root password=antonio host=127.0.0.1 port=3307");
 
     // Crear un repositorio:
     EmpleadoRepository repo(sql);
