e12d8043a7c847255238598d1c9da8d2a3a2234e
aldebarran22 <antonioespinherranz@hotmail.com>
2026-06-11 12:19:16 +0200

    87

--- codigo/seguridad/jwt_tokens/jwt_tokens.cpp
+++ codigo/seguridad/jwt_tokens/jwt_tokens.cpp
@@ -21,20 +21,32 @@ int main()
     crow::SimpleApp app;
 
     CROW_ROUTE(app, "/login").methods("POST"_method)([](const crow::request& request) {
-        auto j = json::parse(request.body);
 
-        // Validar que vienes los campos que necesitamos:
-        if (!j.contains("user") || !j.contains("pwd")) {
-            return crow::response(400, "json: incorrecto, se esperaban campos");
+        try {
+            auto j = json::parse(request.body);
+
+            // Validar que vienes los campos que necesitamos:
+            if (!j.contains("user") || !j.contains("pwd")) {
+                return crow::response(400, "json: incorrecto, se esperaban campos");
+            }
+
+            // Comprobar si existe o no el usuario:
+            if (USER == j.at("user") && PWD == j.at("pwd")) {
+                // Generar el token
+                auto token = jwt::create().
+                    set_issuer("curso C++").
+
+                    set_expires_at(std::chrono::system_clock::now() + std::chrono::minutes{ 30 }).
+                    sign(jwt::algorithm::hs256{ PWD });
+
+
+            }
+            else {
+                return crow::response(401, "No existe el usuario");
+            }
         }
-
-        // Comprobar si existe o no el usuario:
-        if (USER == j.at("user") && PWD == j.at("pwd")) {
-            // Generar el token
-
-        }
-        else {
-            return crow::response(400, "No existe el usuario");
+        catch (const std::exception& e) {
+            return crow::response(500, "Error: " + std::string(e.what()));
         }
     });
 
