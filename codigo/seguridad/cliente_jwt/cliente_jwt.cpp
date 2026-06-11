d2146660f841c8e0314d0b7cc82431b6dca1637f
aldebarran22 <antonioespinherranz@hotmail.com>
2026-06-11 12:42:32 +0200

    87

--- codigo/seguridad/cliente_jwt/cliente_jwt.cpp
+++ codigo/seguridad/cliente_jwt/cliente_jwt.cpp
@@ -9,4 +9,20 @@
 int main()
 {
     std::string datos = "{\"user\":\"admin\",\"pwd\" : \"1234\"}";
+
+    // Peticion POST:
+    try {
+
+        auto response = cpr::Post(cpr::Url{ "http://localhost:8000/login" },
+            cpr::Header{ {"Accept","application/json"} },
+            cpr::Body{ datos });
+
+        // Respuesta:
+        std::string respuesta = response.text;
+        std::cout << respuesta << std::endl;
+
+    }
+    catch (const std::exception& e) {
+        std::cerr << e.what();
+    }
 }
