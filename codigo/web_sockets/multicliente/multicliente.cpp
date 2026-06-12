5eb0da9f80a54cff7e4df8992c09b4499a785c26
aldebarran22 <antonioespinherranz@hotmail.com>
2026-06-12 09:31:52 +0200

    87

--- codigo/web_sockets/multicliente/multicliente.cpp
+++ codigo/web_sockets/multicliente/multicliente.cpp
@@ -2,9 +2,24 @@
 //
 
 #include <iostream>
+#include <vector>
+#include <thread>
+#include <memory>
+#include <string>
+
+#include <boost/beast/core.hpp>
+#include <boost/asio.hpp>
+#include <boost/beast/http.hpp>
+#include <boost/beast/version.hpp>
+
+namespace beast = boost::beast;
+namespace websocket = beast::websocket;
+namespace net = boost::asio;
+namespace http = beast::http;
+using tcp = net::ip::tcp;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+   
 }
 
