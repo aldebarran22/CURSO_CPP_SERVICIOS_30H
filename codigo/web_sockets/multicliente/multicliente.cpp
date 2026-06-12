f95e58e3dca4b292657df20f480b753bc6b06fb8
aldebarran22 <antonioespinherranz@hotmail.com>
2026-06-12 10:07:24 +0200

    87

--- codigo/web_sockets/multicliente/multicliente.cpp
+++ codigo/web_sockets/multicliente/multicliente.cpp
@@ -31,9 +31,65 @@ public:
 	void start() { read_request(); }
 
 private:
-	void read_request(){}
-	void handler_request(){}
-	void write_response(){}
+	void read_request(){
+		// Utiliza una lectura asincrona
+
+		// Coger un shared_ptr que apunta a la clase actual
+		auto self = shared_from_this();
+
+		// La lectura:
+		http::async_read(socket_, buffer, request, [self](beast::error_code ec, std::size_t) {
+
+			if (!ec) {
+				// Si no hay error, manejar la peticion
+				self->handler_request();
+			}
+		});
+
+
+	}
+
+	void handler_request(){
+		// Analizar la peticion:
+
+		std::string respuesta;
+
+		// Recoger la peticion y el metodo HTTP:
+		respuesta = "URL: " + std::string(request.target()) + " Method: " + std::string(request.method_string());
+
+		// Montar la respuesta:
+		response.version(request.version());
+		response.keep_alive(request.keep_alive());
+		response.result(http::status::ok);
+
+		// Configurar las cab de HTTP:
+		response.set(http::field::server, "Boost.asio");
+		response.set(http::field::content_type, "text/plain");
+
+		// Cargar la respuesta:
+		response.body() = respuesta;
+
+		// Calcular el tamaño de la respuesta:
+		response.prepare_payload();
+
+		// Escribir al cliente:
+		this->write_response();
+
+	}
+	void write_response(){
+		// Escribe la respuesta de forma asincrona y cerrar el socket de la conexion
+		
+		// Coger un shared_ptr que apunta a la clase actual
+		auto self = shared_from_this();
+
+		// Escribir de forma asincrona
+		http::async_write(socket_, response, [self](beast::error_code ec, std::size_t) {
+
+			// Cerrar la conexion:
+			self->socket_.shutdown(tcp::socket::shutdown_send, ec);
+		});
+
+	}
 };
 
 void do_accept(tcp::acceptor& acceptor) {
@@ -44,7 +100,7 @@ void do_accept(tcp::acceptor& acceptor) {
 		// Chequear si se ha producido algun error en la conexion, si no hay error:
 		if (!ec) {
 			// Crear e iniciar una nueva session pasando el socket:
-			std::make_shared<session>(std::move(socket)->start());
+			std::make_shared<session>(std::move(socket))->start();
 		}
 
 		// Llamada recursiva para seguir recibiendo cliente:
