681fe7cb1220396059f1bae7428624a6c876e272
aldebarran22 <antonioespinherranz@hotmail.com>
2026-06-22 14:34:30 +0200

    87

---
+++ CODIGO/crow_json/crow_basico/Pedido.h
@@ -0,0 +1,42 @@
+#pragma once
+
+#include <string>
+#include <nlohmann/json.hpp>
+using json = nlohmann::json;
+
+class Pedido
+{
+
+public:
+	int idpedido;
+	std::string cliente;
+	std::string empresa;
+	std::string empleado;
+	double importe;
+	std::string pais;
+
+	Pedido();
+	Pedido(std::string, char =';');
+		
+	~Pedido();
+};
+
+inline void to_json(json& j, const Pedido& p) {
+	j = json{
+		{"idpedido", p.idpedido},
+		{"cliente", p.cliente },
+		{ "empleado", p.empleado },
+		{ "empresa", p.empresa },
+		{ "importe", p.importe },
+		{ "pais", p.pais }
+	};
+}
+inline void from_json(const nlohmann::json& j, Pedido& p) {
+	j.at("idpedido").get_to(p.idpedido);
+	j.at("cliente").get_to(p.cliente);
+	j.at("empleado").get_to(p.empleado);
+	j.at("empresa").get_to(p.empresa);
+	j.at("importe").get_to(p.importe);
+	j.at("pais").get_to(p.pais);
+}
+
