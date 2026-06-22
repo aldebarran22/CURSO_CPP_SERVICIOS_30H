3735e77f5586decf5504c4342a337f548c5f2323
aldebarran22 <antonioespinherranz@hotmail.com>
2026-06-22 13:15:34 +0200

    87

--- CODIGO/crow_json/crow_crud_mem/CrowCRUD.cpp
+++ CODIGO/crow_json/crow_crud_mem/CrowCRUD.cpp
@@ -27,15 +27,62 @@ void CrowCRUD::run(){
     });
 
     CROW_ROUTE(app, "/usuarios").methods(crow::HTTPMethod::GET)([this]() {
-        return crow::response("");
+        // Definimos una estructura lista que se convierte a un array de json
+        crow::json::wvalue lista = crow::json::wvalue::list();
+        int i = 0;
+
+        for (const auto& [id, usuario] : this->usuarios) {
+            crow::json::wvalue item;
+            item["id"] = id;
+
+            if (usuario.has("nombre"))
+                item["nombre"] = usuario["nombre"].s();
+            else
+                item["nombre"] = "";
+
+            item["edad"] = usuario["edad"].i();
+
+            // Cargar en la lista:
+            lista[i++] = std::move(item);
+        }
+
+
+        // Definir la respuesta:
+        crow::json::wvalue  resp;
+        resp["usuarios"] = std::move(lista);
+        return crow::response(resp);
         });
 
     CROW_ROUTE(app, "/usuarios/<int>").methods(crow::HTTPMethod::Delete)([this](int id) {
-        return crow::response("");
+
+        std::lock_guard<std::mutex> lock(this->mtx);
+        if (this->usuarios.erase(id) == 0) {
+            return crow::response(404, "Usuario id: " + std::to_string(id) + " no existe");
+        }
+
+        crow::response resp;
+        resp.code = 204;
+        return resp;
         });
 
     CROW_ROUTE(app, "/usuarios/<int>").methods(crow::HTTPMethod::PUT)([this](const crow::request& req, int id) {
-        return crow::response("");
+        std::lock_guard<std::mutex> lock(this->mtx);
+
+        // Recoger el json que viene de la Request: el tipo es: crow::json::rvalue
+        auto body = crow::json::load(req.body);
+        if (!body) {
+            // 400 Bad Request, el json no coincide con el json esperado:
+            return crow::response(400, "Json incorrecto");
+        }
+
+        // Controlar si existe o no el id en la coleccion:
+        if (usuarios.count(id) == 0) {
+            return crow::response(404, "Usuario con el id: " + std::to_string(id) + " no existe");
+        }
+
+        // Todo ok, actualizamos el usuario:
+        usuarios[id] = std::move(body);
+        return crow::response(200, "Usuario actualizado");
         });
     
     CROW_ROUTE(app, "/usuarios").methods(crow::HTTPMethod::POST)([this](const crow::request& req) {
