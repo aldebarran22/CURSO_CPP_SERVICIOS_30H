e37916f97afafbe5ba40e032d4bab614c87a7f78
aldebarran22 <antonioespinherranz@hotmail.com>
2026-06-22 14:16:59 +0200

    87

--- CODIGO/crow_json/csv_json/csv_json.cpp
+++ CODIGO/crow_json/csv_json/csv_json.cpp
@@ -2,9 +2,33 @@
 //
 
 #include <iostream>
+#include <fstream>
+#include <sstream>
+#include <vector>
+#include <nlohmann/json.hpp>
+
+#include "Pedido.h"
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	std::string pathIn = "..\\..\\practicas\\ficheros\\pedidos_final.csv";
+	std::string pathOut = "..\\..\\practicas\\ficheros\\out\\pedidos.json";
+
+	// Ficheros:
+	std::ifstream fin(pathIn);
+	std::ofstream fout(pathOut);
+
+	std::string linea;
+	std::vector<std::string> campos;
+	std::vector<nlohmann::json> array;
+	nlohmann::json doc;
+
+	if (!fin) {
+		std::cerr << "Fichero no encontrado: " << pathIn << std::endl;
+		return 1;
+	}
+
+
+
 }
 
