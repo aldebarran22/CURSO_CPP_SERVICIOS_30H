6686c22e7be6fa7a276fa58f2fc9d3df6f7b5061
aldebarran22 <antonioespinherranz@hotmail.com>
2026-06-22 13:08:06 +0200

    87

--- CODIGO/crow_json/crow_crud_mem/CrowCRUD.cpp
+++ CODIGO/crow_json/crow_crud_mem/CrowCRUD.cpp
@@ -9,6 +9,32 @@ void CrowCRUD::run(){
 
     crow::SimpleApp app;
 
+
+    CROW_ROUTE(app, "/usuarios/<int>").methods(crow::HTTPMethod::GET)([this](int id) {
+
+        std::lock_guard<std::mutex> lock(mtx);
+        if (this->usuarios.count(id) == 0) {
+            return crow::response(404, "El id: "+std::to_string(id)+" no existe");
+        }
+
+        crow::json::wvalue resp;
+        resp["id"] = id;
+        resp["nombre"] = usuarios[id]["nombre"].s();
+        resp["email"] = usuarios[id]["email"].s();
+        resp["edad"] = usuarios[id]["edad"].i();
+
+        return crow::response(resp);
+    });
+
+    CROW_ROUTE(app, "/usuarios").methods(crow::HTTPMethod::GET)([this]() {
+
+        });
+
+    CROW_ROUTE(app, "/usuarios/<int>").methods(crow::HTTPMethod::Delete)([this](int id) {
+        });
+
+    CROW_ROUTE(app, "/usuarios/<int>").methods(crow::HTTPMethod::PUT)([this](const crow::request& req, int id) {
+        });
     
     CROW_ROUTE(app, "/usuarios").methods(crow::HTTPMethod::POST)([this](const crow::request& req) {
         // Crear un recurso en el servidor:
--- CODIGO/crow_json/crow_crud_mem/crow_crud_mem.cpp
+++ CODIGO/crow_json/crow_crud_mem/crow_crud_mem.cpp
@@ -1,10 +1,13 @@
 // crow_crud_mem.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
 //
 
+#include "CrowCRUD.h"
 #include <iostream>
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    CrowCRUD servidor;
+    servidor.run();
+
 }
 
