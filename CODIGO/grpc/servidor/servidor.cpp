a4fe6e98c75965772dc023c0c360601806dbb127
aldebarran22 <antonioespinherranz@hotmail.com>
2026-06-23 10:17:14 +0200

    87

---
+++ CODIGO/grpc/servidor/servidor.cpp
@@ -0,0 +1,12 @@
+// servidor.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
+//
+
+#include <iostream>
+
+#include "saludo.grpc.pb.h"
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
