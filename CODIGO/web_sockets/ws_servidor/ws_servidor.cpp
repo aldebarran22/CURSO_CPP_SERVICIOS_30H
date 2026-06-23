1e071267b0652fdef161ddb9078193ce9b0b27d3
aldebarran22 <antonioespinherranz@hotmail.com>
2026-06-23 12:31:00 +0200

    87

--- CODIGO/web_sockets/ws_servidor/ws_servidor.cpp
+++ CODIGO/web_sockets/ws_servidor/ws_servidor.cpp
@@ -8,8 +8,46 @@
 #include <boost/beast/websocket.hpp>
 #include <boost/asio.hpp>
 
+namespace beast = boost::beast;
+namespace websocket = beast::websocket;
+namespace net = boost::asio;
+using tcp = net::ip::tcp;
+
 int main()
 {
-    std::cout << "Hello World!\n";
+    // Crear el contexto de comunicación:
+    net::io_context ioc;
+
+    // Definir el endpoint
+    tcp::acceptor acceptor(ioc, tcp::endpoint(tcp::v4(), 80));
+
+    // Capturar el número de hilos máximo:
+    unsigned int numHilos = std::thread::hardware_concurrency();
+
+    // Definir el pool de hilos:
+    boost::asio::thread_pool pool(numHilos);
+
+    std::cout << "Servidor ok, con " << numHilos << " hilos" << std::endl;
+
+    // Esperamos clientes dentro de un bucle inf.
+    while (true) {
+
+        // Definir un socket por cada conexión:
+        tcp::socket socket(ioc);
+
+        std::cout << "Esperando clientes ..." << std::endl;
+
+        // Aceptar el cliente que se conecta:
+        acceptor.accept(socket);
+
+        // Lanzar un hilo para atender el cliente conectado:
+        boost::asio::post(pool, [s = std::move(socket)]() {
+            // Llamar a la tarea (función) que realiza el thread.
+            do_session(std::move(s));
+        });
+
+    }
+
+
 }
 
