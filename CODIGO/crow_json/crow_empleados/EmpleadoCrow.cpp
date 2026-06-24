ae9600e9f1fe8ad3171b50b39797d9ebdb346785
aldebarran22 <antonioespinherranz@hotmail.com>
2026-06-24 14:51:35 +0200

    12

--- CODIGO/crow_json/crow_empleados/EmpleadoCrow.cpp
+++ CODIGO/crow_json/crow_empleados/EmpleadoCrow.cpp
@@ -22,7 +22,7 @@ void EmpleadoCrow::run()
 			return crow::response(doc.dump());
 		}
 		else {
-			crow::response(404, "El empleado con id: " + std::to_string(id));
+			crow::response(404, std::string("El empleado con id: ") + std::to_string(id));
 		}
 	});
 
