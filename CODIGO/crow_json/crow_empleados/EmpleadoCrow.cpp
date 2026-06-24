a91aff56fcda5e0e4bb03461668609115f5180ce
aldebarran22 <antonioespinherranz@hotmail.com>
2026-06-24 14:50:42 +0200

    12

--- CODIGO/crow_json/crow_empleados/EmpleadoCrow.cpp
+++ CODIGO/crow_json/crow_empleados/EmpleadoCrow.cpp
@@ -1,5 +1,10 @@
+#include <crow.h>
+#include <optional>
+#include <nlohmann/json.hpp>
+
 #include "EmpleadoCrow.h"
 
+using json = nlohmann::json;
 
 EmpleadoCrow::EmpleadoCrow(EmpleadoService& service):service(service)
 {
@@ -7,7 +12,21 @@ EmpleadoCrow::EmpleadoCrow(EmpleadoService& service):service(service)
 
 void EmpleadoCrow::run()
 {
+	crow::SimpleApp app;
 
+	CROW_ROUTE(app, "/empleados/<int>").methods(crow::HTTPMethod::GET)([this](int id) {
+
+		auto empleado = this->service.read(id);
+		if (empleado) {
+			json doc = empleado;
+			return crow::response(doc.dump());
+		}
+		else {
+			crow::response(404, "El empleado con id: " + std::to_string(id));
+		}
+	});
+
+	app.port(8080).run();
 }
 
 EmpleadoCrow::~EmpleadoCrow()
