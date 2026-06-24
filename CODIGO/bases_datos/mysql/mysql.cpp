c4dc81a5c1c1f07dffefcf67cafafe9e8cab405a
aldebarran22 <antonioespinherranz@hotmail.com>
2026-06-24 11:38:15 +0200

    87

--- CODIGO/bases_datos/mysql/mysql.cpp
+++ CODIGO/bases_datos/mysql/mysql.cpp
@@ -2,11 +2,31 @@
 //
 
 #include <iostream>
+#include <optional>
+#include <vector>
 #include <soci/soci.h>
 #include <soci/mysql/soci-mysql.h>
+
+#include "Empleado.h"
+#include "EmpleadoRepositorio.h"
 
 int main()
 {
     soci::session sql(soci::mysql, "db=empresa3 user=antonio password=antonio host=127.0.0.1 port=3307");
+    EmpleadoRepositorio repo(sql);
+
+    std::optional<Empleado> emp = repo.read(1);
+    if (emp) {
+        std::cout << emp->nombre << " " << emp->cargo << std::endl;
+    }
+    else {
+        std::cout << "No existe el empleado" << std::endl;
+    }
+
+    std::vector<Empleado> empleados = repo.select();
+    for (const auto& e : empleados) {
+        std::cout << emp->nombre << " " << emp->cargo << std::endl;
+    }
+
 }
 
