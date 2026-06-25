bbe0c89d5650dac58c86a0aa424abcfb38e3e694
aldebarran22 <antonioespinherranz@hotmail.com>
2026-06-25 13:45:57 +0200

    12

--- CODIGO/concurrencia_memoria/hilos_join/hilos_join.cpp
+++ CODIGO/concurrencia_memoria/hilos_join/hilos_join.cpp
@@ -17,15 +17,31 @@ void generarAleatorios(int n, int limite, int mili, int& suma) {
     }
 }
 
+class Hilo {
+    int n;
+    int tiempo;
+
+public:
+    Hilo(int n=5, int tiempo=1): n(n), tiempo(tiempo){}
+
+    void operator()() {
+        for (int i = 0; i < n; i++) {
+            std::cout << "Operador(): " << i << ": " << std::endl;
+            std::this_thread::sleep_for(std::chrono::seconds(tiempo));
+        }
+    }
+};
+
 int main()
 {
     int suma = 0;
     int nMensajes = 6;
     int ml = 350;
+    Hilo hilo;
 
     // Crear un hilo a partir de una función, pasando parámetros por copia y por referencia:
     std::thread hiloFuncion{ generarAleatorios, 10, 250, 500, std::ref(suma) };
-    hiloFuncion.join();
+    
 
     std::thread hiloLambda([nMensajes, ml]() {
         for (int i = 0; i < nMensajes; i++) {
@@ -33,7 +49,14 @@ int main()
             std::this_thread::sleep_for(std::chrono::milliseconds(ml));
         }
     });
+
+    std::thread hiloObjeto(hilo);
+
+
+    // Esperar a que terminen los hilos
+    hiloFuncion.join();
     hiloLambda.join();
+    hiloObjeto.join();
 
     std::cout << "La suma de aleatorios es:  " << suma << std::endl;
 
