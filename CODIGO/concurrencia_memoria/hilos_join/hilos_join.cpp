6872926dc329f0b0b43f66cd3b8381f12387d5f8
aldebarran22 <antonioespinherranz@hotmail.com>
2026-06-25 13:39:04 +0200

    12

--- CODIGO/concurrencia_memoria/hilos_join/hilos_join.cpp
+++ CODIGO/concurrencia_memoria/hilos_join/hilos_join.cpp
@@ -20,10 +20,20 @@ void generarAleatorios(int n, int limite, int mili, int& suma) {
 int main()
 {
     int suma = 0;
+    int nMensajes = 6;
+    int ml = 350;
 
     // Crear un hilo a partir de una función, pasando parámetros por copia y por referencia:
     std::thread hiloFuncion{ generarAleatorios, 10, 250, 500, std::ref(suma) };
     hiloFuncion.join();
+
+    std::thread hiloLambda([nMensajes, ml]() {
+        for (int i = 0; i < nMensajes; i++) {
+            std::cout << "mensaje lambda: " << (i + 1) << std::endl;
+            std::this_thread::sleep_for(std::chrono::milliseconds(ml));
+        }
+    });
+    hiloLambda.join();
 
     std::cout << "La suma de aleatorios es:  " << suma << std::endl;
 
