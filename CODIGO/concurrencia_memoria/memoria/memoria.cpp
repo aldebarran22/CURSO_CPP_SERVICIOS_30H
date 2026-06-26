02e7e4073adeb279984edb9603f7307785a85598
aldebarran22 <antonioespinherranz@hotmail.com>
2026-06-26 09:07:00 +0200

    87

---
+++ CODIGO/concurrencia_memoria/memoria/memoria.cpp
@@ -0,0 +1,22 @@
+// memoria.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
+//
+
+#include <iostream>
+
+void conPunteros() {
+    int** ptr = nullptr;
+
+    ptr = new int* [10];
+    for (int i = 0; i < 10; i++) {
+        ptr[i] = new int[10];
+    }
+
+    delete[] ptr;
+}
+
+
+int main()
+{
+    conPunteros();
+}
+
