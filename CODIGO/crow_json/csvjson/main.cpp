a826b58816061338785ddd4702bcf932aff00dea
aldebarran22 <56862985+aldebarran22@users.noreply.github.com>
2026-07-13 12:13:10 +0200

    2

--- CODIGO/crow_json/csvjson/main.cpp
+++ CODIGO/crow_json/csvjson/main.cpp
@@ -6,6 +6,8 @@
 #include <vector>
 #include <nlohmann/json.hpp>
 
+#include "Pedido.h"
+
 int main()
 {
 	std::string pathIn = "..\\..\\..\\practicas\\ficheros\\pedidos_final.csv";
@@ -13,6 +15,9 @@ int main()
 
 	std::ifstream fin(pathIn);
 	std::ofstream fout(pathOut);
+	std::vector<nlohmann::json> array; // Almacenar los docs de json (1 por pedido)
+	nlohmann::json doc; // Documento final de json, convierte automaticamente el vector -> array json
+	bool cabs = true;
 
 	std::string linea;
 
@@ -22,8 +27,26 @@ int main()
 	}
 
 	while (std::getline(fin, linea)) {
-		std::cout << linea << std::endl;
+		//std::cout << linea << std::endl;
+
+		if (cabs) {
+			cabs = false;
+		}
+		else {
+			Pedido p(linea);
+			// Convertir el pedido a json:
+			array.push_back(p.to_json());
+		}
 	}
+
+	// Convertir el vector en un array:
+	doc = array;
+
+	// A la consola:
+	std::cout << doc.dump(4) << std::endl;
+
+	// Al fichero json:
+	fout << doc.dump(4);
 
 	fin.close();
 	fout.close();
