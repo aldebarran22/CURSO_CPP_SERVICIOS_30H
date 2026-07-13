7acd5a7f05254507fca1b93cd76d509f4620862f
aldebarran22 <56862985+aldebarran22@users.noreply.github.com>
2026-07-13 11:53:50 +0200

    87

---
+++ CODIGO/crow_json/csvjson/Utilidades.cpp
@@ -0,0 +1,14 @@
+#include <sstream>
+
+#include "Utilidades.h"
+
+void Utilidades::split(const std::string& s, char delim, std::vector<std::string>& elems)
+{
+	std::stringstream ss;
+	ss.str(s);
+	std::string item;
+
+	while (std::getline(ss, item, delim)) {
+		elems.push_back(item);
+	}
+}
