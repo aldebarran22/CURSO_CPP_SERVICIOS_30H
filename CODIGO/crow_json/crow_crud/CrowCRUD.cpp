fd0dacc4a2a5a55c6940beadb72ca897ca7b69ef
aldebarran22 <56862985+aldebarran22@users.noreply.github.com>
2026-07-13 14:55:04 +0200

    87

--- CODIGO/crow_json/crow_crud/CrowCRUD.cpp
+++ CODIGO/crow_json/crow_crud/CrowCRUD.cpp
@@ -1,3 +1,5 @@
+#include <thread>
+
 #include "CrowCRUD.h"
 
 CrowCRUD::CrowCRUD()
@@ -7,7 +9,31 @@ CrowCRUD::CrowCRUD()
 
 void CrowCRUD::run()
 {
+	crow::SimpleApp app;
 
+	CROW_ROUTE(app, "/usuarios").methods(crow::HTTPMethod::POST)([this](const crow::request& req)) {
+		
+		// Recoger el json de la petición:
+		auto body = crow::json::load(req.body);
+
+		if (!body) {
+			return crow::response(400, "json incorrecto");
+		}
+
+		// Guardar el usuario en el mapa (en memoria)
+		// Bloquea hacia abajo
+		std::lock_guard<std::mutex> lock(this->mtx);
+		int id = this->siguiente_id++;
+		this->usuarios[id] = std::move(body); // OJO, no copiar -> mover!!!
+
+		// Montar la respuesta al cliente:
+		crow::json::wvalue resp;
+		resp["id"] = id;
+		return crow::response(201, resp);
+
+	});
+
+	app.port(8080).multithreaded().concurrency(std::thread::hardware_concurrency()).run();
 }
 
 CrowCRUD::~CrowCRUD()
