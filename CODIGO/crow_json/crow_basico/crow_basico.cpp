6cf2b133ff7f3470ba23351f41ae8755c6b1a8c8
aldebarran22 <56862985+aldebarran22@users.noreply.github.com>
2026-07-13 12:52:01 +0200

    87

---
+++ CODIGO/crow_json/crow_basico/crow_basico.cpp
@@ -0,0 +1,24 @@
+// crow_basico.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
+//
+
+#include <iostream>
+#include <crow.h>
+#include <thread>
+#include <string>
+
+int main()
+{
+    // Objeto principal de crow: define operaciones y poner en marchar el server!
+    crow::SimpleApp app;
+
+
+    // Definir operaciones:
+    // CROW_ROUTE(app, url/<tipo1>/<tipo2>).methods(...)([](tipo1 p1, tipo2 p2){cuerpo});
+
+    CROW_ROUTE(app, "/")([]() {
+        return "Hello World";
+    });
+
+
+    // Poner en marcha el servidor:
+}
