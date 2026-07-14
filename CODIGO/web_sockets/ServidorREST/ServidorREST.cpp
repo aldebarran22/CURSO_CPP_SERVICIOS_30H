f857e478eef830c6b60e9d453c1240378463bc0f
aldebarran22 <56862985+aldebarran22@users.noreply.github.com>
2026-07-14 12:46:03 +0200

    78

--- CODIGO/web_sockets/ServidorREST/ServidorREST.cpp
+++ CODIGO/web_sockets/ServidorREST/ServidorREST.cpp
@@ -21,6 +21,7 @@ void ServidorREST::run()
 
         // Aceptar peticion:
         acceptor.accept(socket);
+        std::cout << "cliente conectado" << std::endl;
 
         // Procesar la peticion:
         procesarPeticion(socket);
@@ -28,8 +29,28 @@ void ServidorREST::run()
 }
 
 
-void ServidorREST::procesarPeticion(tcp::socket&)
+void ServidorREST::procesarPeticion(tcp::socket& socket)
 {
+    beast::flat_buffer buffer;
+    http::request<http::string_body> request;
+    std::string mensaje = "";
+    std::string metodo;
+    std::string target;
+
+    try {
+        // Leer la peticion:
+        http::read(socket, buffer, request);
+
+        // Extraer la url y el metodo:
+        target = request.target();
+        metodo = request.method_string();
+
+        std::cout << "Peticion: " << target << " metodo: " << metodo << std::endl;
+
+    }
+    catch (const std::exception& e) {
+
+    }
 }
 
 std::string ServidorREST::peticionGET()
