0ddcc7ae7319a5a4e2a9c163c32bfc21af2d1bb5
aldebarran22 <56862985+aldebarran22@users.noreply.github.com>
2026-07-14 10:39:47 +0200

    87

--- CODIGO/grpc/servidor/servidor.cpp
+++ CODIGO/grpc/servidor/servidor.cpp
@@ -2,6 +2,38 @@
 //
 
 #include <iostream>
+#include <grpcpp/grpcpp.h>
+
+#include "saludo.grpc.pb.h"
+
+using grpc::Server;
+using grpc::ServerBuilder;
+using grpc::ServerContext;
+using grpc::Status;
+
+// Del fichero proto:
+using saludo::Saludo;
+using saludo::Solicitud;
+using saludo::Respuesta;
+
+// Clase que hereda de Service y da contenido al método virtual puro DiHola
+class SaludoServiceImpl final : public Saludo::Service {
+
+    Status DiHola(ServerContext* context, const Solicitud* request, Respuesta* response) override {
+        // Programar nuestra logica de negocio en el metodo:
+
+        // Recoger el nombre que viene de la solicitud:
+        std::string nombre = request->nombre();
+
+        // Montar el mensaje de respuesta:
+        response->set_mensaje("hola " + nombre);
+
+        return Status::OK;
+    }
+
+};
+
+
 
 int main()
 {
