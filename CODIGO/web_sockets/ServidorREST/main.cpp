7bb14ca86003c726a47fcb98d9908b85d2084748
aldebarran22 <56862985+aldebarran22@users.noreply.github.com>
2026-07-14 12:48:04 +0200

    87

--- CODIGO/web_sockets/ServidorREST/main.cpp
+++ CODIGO/web_sockets/ServidorREST/main.cpp
@@ -1,7 +1,13 @@
 #include <iostream>
+#include <boost/asio.hpp>
+
 #include "ServidorREST.h"
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	// Crear el contexto de E/S:
+	boost::asio::io_context ioc;
+
+	ServidorREST server(ioc, 8080);
+	server.run();
 }
