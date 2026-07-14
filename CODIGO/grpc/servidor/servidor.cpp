f3776f3e10b34ba6355ae61b23071b0c8b2b1c47
aldebarran22 <56862985+aldebarran22@users.noreply.github.com>
2026-07-14 11:50:34 +0200

    87

--- CODIGO/grpc/servidor/servidor.cpp
+++ CODIGO/grpc/servidor/servidor.cpp
@@ -6,14 +6,6 @@
 #include <memory>
 
 #include <grpcpp/grpcpp.h>
-
-/*
-#pragma warning(push)
-#pragma warning(disable : 4996)
-
-#pragma warning(pop)*/
-
-
 #include "..\saludo.grpc.pb.h"
 
 using grpc::Server;
