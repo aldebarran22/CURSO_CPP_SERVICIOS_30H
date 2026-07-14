d5a062b2672f688357187b366edc0216cc83cc7c
aldebarran22 <56862985+aldebarran22@users.noreply.github.com>
2026-07-14 12:17:36 +0200

    87

---
+++ CODIGO/web_sockets/ServidorREST/ServidorREST.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include <boost/beast/core.hpp>
+#include <boost/beast/http.hpp>
+#include <boost/beast/version.hpp>
+#include <boost/asio/ip/tcp.hpp>
+
+#include <iostream>
+#include <unordered_map>
+#include <string>
+
+#include <nlohmann/json.hpp>
+
+class ServidorREST
+{
+};
+
