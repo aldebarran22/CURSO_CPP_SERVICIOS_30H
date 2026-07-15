373f781a5c19cacd0c27b10bc3136c6549df8912
aldebarran22 <56862985+aldebarran22@users.noreply.github.com>
2026-07-15 10:19:21 +0200

    87

--- CODIGO/base_datos/mysql/EmpleadoRepositorio.cpp
+++ CODIGO/base_datos/mysql/EmpleadoRepositorio.cpp
@@ -1,1 +1,34 @@
 #include "EmpleadoRepositorio.h"
+
+EmpleadoRepositorio::EmpleadoRepositorio(soci::session& sql)
+{
+}
+
+std::optional<Empleado> EmpleadoRepositorio::read(int id)
+{
+	return std::optional<Empleado>();
+}
+
+bool EmpleadoRepositorio::create(const Empleado& emp)
+{
+	return false;
+}
+
+bool EmpleadoRepositorio::_delete(int id)
+{
+	return false;
+}
+
+bool EmpleadoRepositorio::update(const Empleado& emp)
+{
+	return false;
+}
+
+std::vector<Empleado> EmpleadoRepositorio::select()
+{
+	return std::vector<Empleado>();
+}
+
+EmpleadoRepositorio::~EmpleadoRepositorio()
+{
+}
