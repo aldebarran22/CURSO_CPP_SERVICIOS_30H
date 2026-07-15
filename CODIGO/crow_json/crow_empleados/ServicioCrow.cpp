4296c547ac3f5af41e143b97597fd3bb6e3b92bb
aldebarran22 <56862985+aldebarran22@users.noreply.github.com>
2026-07-15 14:58:34 +0200

    12

--- CODIGO/crow_json/crow_empleados/ServicioCrow.cpp
+++ CODIGO/crow_json/crow_empleados/ServicioCrow.cpp
@@ -5,6 +5,9 @@
 
 #include "ServicioCrow.h"
 
+using json = nlohmann::json;
+
+
 ServicioCrow::ServicioCrow(EmpleadoService& service):service(service)
 {
 }
@@ -22,6 +25,30 @@ void ServicioCrow::run()
 		}
 		else {
 			return crow::response(404, std::string("El empleado con id: " + std::to_string(id)));
+		}
+	});
+
+	CROW_ROUTE(app, "/empleados").methods(crow::HTTPMethod::POST)([this](const crow::request& req) {
+
+		try {
+			// Recoger el json de la petición:
+			auto j = json::parse(req.body);
+
+			// Validacion del json:
+			if (!j.contains("id") || !j.contains("nombre") || !j.contains("cargo")) {
+				return crow::response(400, "Json incorrecto. Faltan etiquetas");
+			}
+
+			Empleado e = j.get<Empleado>();
+			if (this->service.create(e)) {
+				return crow::response("Empleado creado");
+			}
+			else {
+				return crow::response(500, "Empleado no creado");
+			}
+		}
+		catch (const std::exception& e) {
+			return crow::response(500, std::string(e.what());
 		}
 	});
 
