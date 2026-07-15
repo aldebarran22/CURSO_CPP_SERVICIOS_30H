cd1aae4424fbfe31babc826481adcd3daf49dab2
aldebarran22 <56862985+aldebarran22@users.noreply.github.com>
2026-07-15 12:35:03 +0200

    12

--- CODIGO/base_datos/mysql/EmpleadoCache.cpp
+++ CODIGO/base_datos/mysql/EmpleadoCache.cpp
@@ -1,12 +1,45 @@
+#include <stdexcept>
+#include <string>
+#include <iostream>
+#include <nlohmann/json.hpp>
+
 #include "EmpleadoCache.h"
 
 EmpleadoCache::EmpleadoCache()
 {
+	std::string mensaje = "";
+
+	this->contexto = redisConnect("127.0.0.1", 6379);
+	if (this->contexto == nullptr || this->contexto->err) {
+		if (this->contexto) {
+			mensaje = this->contexto->errstr;
+		}
+		throw std::runtime_error("Error al conectar con redis "+mensaje);
+	}
 }
 
 std::optional<Empleado> EmpleadoCache::getEmpleado(int id)
 {
-	return std::optional<Empleado>();
+	std::string key = "empleado" + std::to_string(id);
+
+	// Buscar la clave:
+	redisReply* reply = (redisReply*)redisCommand(this->contexto, "GET %s", key.c_str());
+
+	if (reply->type == REDIS_REPLY_STRING) {
+		// La hemos encontrado:
+		std::string cadjson = reply->str;
+		json j = json::parse(cadjson);
+
+		Empleado e = j.get<Empleado>();
+		std::cout << "Recupera " << id << " de la cache" << std::endl;
+
+		freeReplyObject(reply);
+		return e;
+	}
+	else {
+		freeReplyObject(reply);
+		return std::nullopt;
+	}
 }
 
 void EmpleadoCache::saveEmpleado(const Empleado& e)
@@ -15,4 +48,8 @@ void EmpleadoCache::saveEmpleado(const Empleado& e)
 
 EmpleadoCache::~EmpleadoCache()
 {
+	if (this->contexto) {
+		redisFree(this->contexto);
+		this->contexto = nullptr;
+	}
 }
