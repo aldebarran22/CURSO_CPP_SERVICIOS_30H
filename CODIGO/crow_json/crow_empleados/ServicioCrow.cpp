26f004994798ee0287fcd561d3a301223430f576
aldebarran22 <56862985+aldebarran22@users.noreply.github.com>
2026-07-15 14:34:16 +0200

    87

--- CODIGO/crow_json/crow_empleados/ServicioCrow.cpp
+++ CODIGO/crow_json/crow_empleados/ServicioCrow.cpp
@@ -1,3 +1,8 @@
+#include <crow.h>
+#include <optional>
+#include <thread>
+#include <nlohmann/json.hpp>
+
 #include "ServicioCrow.h"
 
 ServicioCrow::ServicioCrow(EmpleadoService& service):service(service)
@@ -6,7 +11,21 @@ ServicioCrow::ServicioCrow(EmpleadoService& service):service(service)
 
 void ServicioCrow::run()
 {
+	crow::SimpleApp app;
 
+	CROW_ROUTE(app, "/empleados/<int>").methods(crow::HTTPMethod::GET)([this](int id) {
+
+		auto empleado = this->service.read(id);
+		if (empleado) {
+			json doc = empleado;
+			return crow::response(doc.dump());
+		}
+		else {
+			return crow::response(404, std::string("El empleado con id: " + std::to_string(id)));
+		}
+	});
+
+	app.port(8080).multithreaded().concurrency(std::thread::hardware_concurrency()).run();
 }
 
 ServicioCrow::~ServicioCrow()
