ad65dc15f9931b06c31aae35b789babca7d8f387
aldebarran22 <56862985+aldebarran22@users.noreply.github.com>
2026-07-15 13:59:33 +0200

    87

--- CODIGO/base_datos/postgresql/main.cpp
+++ CODIGO/base_datos/postgresql/main.cpp
@@ -11,7 +11,7 @@
 int main()
 {
     try {
-        const char* cadConex = "host=127.0.0.1 port=5433 dbname=empresa user=antonio password=antonio";
+        const char* cadConex = "host=127.0.0.1 port=5433 dbname=empresa3 user=antonio password=antonio";
         EmpleadoRepository repo(cadConex);
         std::cout << "Conexion ok" << std::endl;
 
