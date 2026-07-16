54051279b4f0b0f4bd44f033f74a27d0e1b6c123
aldebarran22 <56862985+aldebarran22@users.noreply.github.com>
2026-07-16 14:40:44 +0200

    87

--- CODIGO/concurrencia_memoria/crear_hilos/crear_hilos.cpp
+++ CODIGO/concurrencia_memoria/crear_hilos/crear_hilos.cpp
@@ -8,7 +8,7 @@
 #include <mutex>
 
 
-void generarAleatorias(int n, int limite, int mili, int& suma) {
+void generarAleatorios(int n, int limite, int mili, int& suma) {
 
     for (int i = 0; i < n; i++) {
         int num = std::rand() % limite;
@@ -19,7 +19,52 @@ void generarAleatorias(int n, int limite, int mili, int& suma) {
     }
 }
 
+class Hilo {
+    int id;
+    int n;
+    int tiempo;
+    std::mutex& m;
+
+public:
+    Hilo(int id, std::mutex& m, int n = 5, int tiempo = 1) :id(id), m(m), n(n), tiempo(tiempo) {}
+
+    void operator()() {
+
+        {
+            std::lock_guard<std::mutex> lock(m);
+            std::cout << "Iniciar el operador(" << id << ") con getId: " << std::this_thread::get_id() << std::endl;
+        }
+
+        for (int i = 0; i < n; i++) {
+            {
+                std::lock_guard<std::mutex> lock(m);
+                std::cout << "Operador(" << id << ")" << " " << i << std::endl;
+            }
+            std::this_thread::sleep_for(std::chrono::seconds(tiempo));
+        }
+
+        {
+            std::lock_guard<std::mutex> lock(m);
+            std::cout << "Termina el operador(" << id << ")" << std::endl;
+        }
+    }
+};
+
 int main()
 {
+    int suma = 0;
+
+    // Crear el hilo con una funcion:
+    std::thread hiloFuncion{ generarAleatorios, 10, 250, 500, std::ref(suma) };
     
+    int nMensajes = 6;
+    int ml = 350;
+
+    std::thread hiloLambda([nMensajes, ml]() {
+        for (int i = 0; i < nMensajes; i++) {
+            std::cout << "mensaje lambda: " << (n + 1) << std::endl;
+            std::this_thread::sleep_for(std::chrono::milliseconds(ml));
+        }
+        });
+
 }
