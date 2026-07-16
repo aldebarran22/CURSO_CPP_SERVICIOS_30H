5f26ef9bbcb9be5ebe2ddf793dcd92b8147501e1
aldebarran22 <56862985+aldebarran22@users.noreply.github.com>
2026-07-16 11:03:45 +0200

    87

--- CODIGO/web_sockets/wss_server/wss_server.cpp
+++ CODIGO/web_sockets/wss_server/wss_server.cpp
@@ -7,19 +7,29 @@
 #include <boost/beast/core.hpp>
 #include <boost/beast/websocket.hpp>
 #include <boost/asio.hpp>
+#include <boost/beast/ssl.hpp>
+
+#define PUERTO 443
 
 namespace beast = boost::beast;
 namespace websocket = beast::websocket;
 namespace net = boost::asio;
 namespace http = boost::beast::http;
+namespace ssl = net::ssl;
+
 using tcp = net::ip::tcp;
 
-void do_session(tcp::socket socket) {
+
+
+
+void do_session(tcp::socket socket, ssl::context& ctx) {
 
     try {
 
         // Definir el websocket: sera un stream de tipo tcp:
-        websocket::stream<tcp::socket> ws(std::move(socket));
+        websocket::stream<beast::ssl_stream<tcp::socket>> ws(std::move(socket), ctx);
+        ws.next_layer().handshake(ssl::stream_base::server);
+
         ws.accept();
 
         for (;;) {
@@ -59,8 +69,16 @@ int main()
     // Crear el contexto de comunicacion;
     net::io_context ioc;
 
+    // Definir un contexto SSL:
+    ssl::context ctx(ssl::context::tlsv12_server);
+
+    // Indicar cuales son los ficheros de los certificados:
+    ctx.use_certificate_chain_file("..\\certificados\\cert.pem");
+    ctx.use_private_key_file("..\\certificados\\key.pem", ssl::context::file_format::pem);
+
+
     // Definir el endpoint con el objeto acceptor:
-    tcp::acceptor acceptor(ioc, tcp::endpoint(tcp::v4(), 80));
+    tcp::acceptor acceptor(ioc, tcp::endpoint(tcp::v4(), PUERTO));
 
     // Capturar el numero de hilos max. soportado:
     unsigned int numHilos = std::thread::hardware_concurrency();
@@ -73,14 +91,14 @@ int main()
         // Definir un socket por cada  conexion:
         tcp::socket socket(ioc);
 
-        std::cout << "Servidor a la espera de clientes ... " << std::endl;
+        std::cout << "Servidor wss a la espera de clientes. Puerto: " << PUERTO << std::endl;
 
         // Aceptar al cliente:
         acceptor.accept(socket);
 
         // Lanzar un hilo para atender al cliente conectado:
-        boost::asio::post(pool, [s = std::move(socket)]() mutable {
-            do_session(std::move(s));
+        boost::asio::post(pool, [s = std::move(socket), &ctx]() mutable {
+            do_session(std::move(s), ctx);
             });
     }
 }
