f91d03102bad83f5095602bcca1af00841f564cb
aldebarran22 <56862985+aldebarran22@users.noreply.github.com>
2026-07-16 12:30:37 +0200

    87

--- CODIGO/crow_json/crow_jwt/crow_jwt.cpp
+++ CODIGO/crow_json/crow_jwt/crow_jwt.cpp
@@ -2,9 +2,32 @@
 //
 
 #include <iostream>
+#include <crow.h>
+#include <nlohmann/json.hpp>
+
+#define USER "admin"
+#define PASS "1234"
+
+using json = nlohmann::json;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	crow::SimpleApp app;
+
+	CROW_ROUTE(app, "/login").methods("POST"_method)([](const crow::request& request) {
+		try {
+			// Recuperar el cuerpo de la peticion:
+			auto j = json::parse(request.body);
+
+			// Validar las etiquetas:
+			if (!j.contains("user") || !j.contains("pass")) {
+				return crow::response(400, "json incorrecto, se esperaban las credenciales");
+			}
+
+		}
+		catch (const std::exception& e) {
+			return crow::response(500, e.what());
+		}
+	});
 }
 
