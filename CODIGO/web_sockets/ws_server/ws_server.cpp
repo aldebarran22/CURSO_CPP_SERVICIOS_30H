7a75552cd9bd77c3a656a7dd064bdaa1e75a05bb
aldebarran22 <56862985+aldebarran22@users.noreply.github.com>
2026-07-16 10:10:59 +0200

    87

--- CODIGO/web_sockets/ws_server/ws_server.cpp
+++ CODIGO/web_sockets/ws_server/ws_server.cpp
@@ -16,5 +16,31 @@ using tcp = net::ip::tcp;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    // Crear el contexto de comunicacion;
+    net::io_context ioc;
+
+    // Definir el endpoint con el objeto acceptor:
+    tcp::acceptor acceptor(ioc, tcp::endpoint(tcp::v4(), 80));
+
+    // Capturar el numero de hilos max. soportado:
+    unsigned int numHilos = std::thread::hardware_concurrency();
+
+    // Definir el pool de hilos: 
+    boost::asio::thread_pool pool(numHilos);
+
+    while (true) {
+
+        // Definir un socket por cada  conexion:
+        tcp::socket socket(ioc);
+
+        std::cout << "Servidor a la espera de clientes ... " << std::endl;
+
+        // Aceptar al cliente:
+        acceptor.accept(socket);
+
+        // Lanzar un hilo para atender al cliente conectado:
+        boost::asio::post(pool, [s = std::move(socket)]() mutable {
+            do_session(std::move(s));
+        });
+    }
 }
