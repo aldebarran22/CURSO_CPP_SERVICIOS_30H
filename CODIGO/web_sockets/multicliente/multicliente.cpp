d98b0dd2f414b7b691466901d706f2e021c389c8
aldebarran22 <56862985+aldebarran22@users.noreply.github.com>
2026-07-17 09:11:02 +0200

    12

--- CODIGO/web_sockets/multicliente/multicliente.cpp
+++ CODIGO/web_sockets/multicliente/multicliente.cpp
@@ -3,10 +3,13 @@
 
 #include <iostream>
 #include <thread>
+#include <vector>
 
 #include <boost/beast/core.hpp>
 #include <boost/beast/websocket.hpp>
 #include <boost/asio.hpp>
+#include <boost/beast/http.hpp>
+#include <boost/beast/version.hpp>
 
 namespace beast = boost::beast;
 namespace websocket = beast::websocket;
@@ -16,6 +19,24 @@ using tcp = net::ip::tcp;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    // Crear el contexto de comunicacion;
+    net::io_context ioc;
+
+    // Definir el endpoint con el objeto acceptor:
+    tcp::acceptor acceptor(ioc, tcp::endpoint(tcp::v4(), 8080));
+
+    do_accept(acceptor);
+
+    // Con varios hilos:
+    std::vector<std::thread> hilos;
+    unsigned int numhilos = std::thread::hardware_concurrency();
+
+    for (int i = 0; i < numhilos; i++) {
+        // Lanzar un contexto io por cada uno de los hilos
+        hilos.emplace_back([&ioc] { ioc.run();  });
+    }
+
+    // Un unico hilo:
+    //ioc.run();
 }
 
