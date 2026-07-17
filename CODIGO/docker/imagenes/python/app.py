f739d803b116a8ac89f8ca86db47cabf5808ed20
aldebarran22 <56862985+aldebarran22@users.noreply.github.com>
2026-07-17 12:21:43 +0200

    87

---
+++ CODIGO/docker/imagenes/python/app.py
@@ -0,0 +1,13 @@
+
+from flask import Flask, request
+
+app = Flask(__name__)
+
+@app.route('/saludo', methods=['GET'])
+def saludo():
+    nombre = request.args.get('nombre', 'Mundo')
+    return f"¡Hola, {nombre}!"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
+
