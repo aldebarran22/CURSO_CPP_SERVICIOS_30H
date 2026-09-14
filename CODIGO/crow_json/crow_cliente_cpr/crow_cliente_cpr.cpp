9118d26fdf8d223d3262261110bceaa1678f244e
aldebarran22 <56862985+aldebarran22@users.noreply.github.com>
2026-09-14 14:31:57 +0200

    87

---
+++ CODIGO/crow_json/crow_cliente_cpr/crow_cliente_cpr.cpp
@@ -0,0 +1,17 @@
+// crow_cliente_cpr.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
+//
+
+#include <iostream>
+#include <nlohmann/json.hpp>
+#include <cpr/cpr.h>
+
+void test_crow_basico() {
+
+    // Peticion GET a 
+}
+
+int main()
+{
+    test_crow_basico();
+}
+
