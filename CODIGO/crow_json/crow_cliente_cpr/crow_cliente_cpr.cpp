586389cd51408583079cda77bc84d57a58bc644d
aldebarran22 <56862985+aldebarran22@users.noreply.github.com>
2026-09-14 14:49:35 +0200

    87

--- CODIGO/crow_json/crow_cliente_cpr/crow_cliente_cpr.cpp
+++ CODIGO/crow_json/crow_cliente_cpr/crow_cliente_cpr.cpp
@@ -8,18 +8,40 @@
 
 void test_crow_basico() {
 
-    // Peticion GET a 
+    // Peticion GET 
     int n1 = 10, n2 = 78;
 
     std::string sUrl = "http://localhost:18000/operacion/" + std::to_string(n1) + "/" + std::to_string(n2);
     cpr::Url url{ sUrl };
 
-    std::cout << "Peticíon GET: " << sUrl << std::endl;
+    std::cout << "Peticion GET: " << sUrl << std::endl;
     cpr::Response resp = cpr::Get(url);
 
     std::cout << "Status: " << resp.status_code << std::endl;
     std::cout << "Body: " << resp.text << std::endl;
     
+    
+    // Peticion POST:
+    std::string sUrl2 = "http://localhost:18000/pedidos";
+    cpr::Url url2{ sUrl2 };
+
+    std::cout << "Peticion POST: " << sUrl2 << std::endl;
+    nlohmann::json j{
+        {"idpedido", "12345"},
+        {"cliente" , "NCH"},
+        {"empleado" , "Jorge"},
+        {"empresa" , "Indra"},
+        {"importe" , 250.0},
+        {"pais" , "España"}
+    };
+
+    cpr::Response resp2 = cpr::Post(url2,
+        cpr::Header {{"Content-type", "application/json"}},
+        cpr::Body{ j.dump() }
+    );
+
+    std::cout << "Status2: " << resp2.status_code << std::endl;
+    std::cout << "Body2: " << resp2.text << std::endl;
 }
 
 int main()
