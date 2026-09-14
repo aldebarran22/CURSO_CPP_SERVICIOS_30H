d1b3537e8257f27812112bff73f4f467c655695c
aldebarran22 <56862985+aldebarran22@users.noreply.github.com>
2026-09-14 14:39:20 +0200

    87

--- CODIGO/crow_json/crow_cliente_cpr/crow_cliente_cpr.cpp
+++ CODIGO/crow_json/crow_cliente_cpr/crow_cliente_cpr.cpp
@@ -4,10 +4,22 @@
 #include <iostream>
 #include <nlohmann/json.hpp>
 #include <cpr/cpr.h>
+#include <string>
 
 void test_crow_basico() {
 
     // Peticion GET a 
+    int n1 = 10, n2 = 78;
+
+    std::string sUrl = "http://localhost:18000/operacion/" + std::to_string(n1) + "/" + std::to_string(n2);
+    cpr::Url url{ sUrl };
+
+    std::cout << "Peticíon GET: " << sUrl << std::endl;
+    cpr::Response resp = cpr::Get(url);
+
+    std::cout << "Status: " << resp.status_code << std::endl;
+    std::cout << "Body: " << resp.text << std::endl;
+    
 }
 
 int main()
