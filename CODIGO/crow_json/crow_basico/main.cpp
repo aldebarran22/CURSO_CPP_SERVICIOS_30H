16e7d285560c94c5ca7998ad41ec37361ce6c6b5
aldebarran22 <56862985+aldebarran22@users.noreply.github.com>
2026-09-14 12:20:29 +0200

    87

--- CODIGO/crow_json/crow_basico/main.cpp
+++ CODIGO/crow_json/crow_basico/main.cpp
@@ -13,8 +13,19 @@ int main()
     // Definir operaciones dentro del servicio:
     // CROW_ROUTE(app, url/<tipo1>/<tipo2>).methods(...)([](tipo1 p1, tipo2 p2){cuerpo});
 
-    CROW_ROUTE(app, "/")([]() {
+    CROW_ROUTE(app, "/")([]() { // por defecto es GET
         return "Hello World";
+    });
+
+    // Ejemplo con parametros: http://localhost:18000/operacion?p1=7&p2=9
+    CROW_ROUTE(app, "/operacion/<int>/<int>").methods(crow::HTTPMethod::GET)([](int a, int b) {
+        crow::json::wvalue resp;
+
+        int suma = a + b;
+        resp["a"] = a;
+        resp["b"] = b;
+        resp["resul"] = suma;
+        return crow::response{ resp };
     });
 
     app.port(18000).multithreaded().concurrency(std::thread::hardware_concurrency());
