63de12fd8c55d08a472d2518426735172c83ccad
aldebarran22 <56862985+aldebarran22@users.noreply.github.com>
2026-09-15 10:11:50 +0200

    87

--- CODIGO/crow_json/crow_crud/CrowCRUD.cpp
+++ CODIGO/crow_json/crow_crud/CrowCRUD.cpp
@@ -34,22 +34,34 @@ void CrowCRUD::run()
 
 	CROW_ROUTE(app, "/usuarios").methods(crow::HTTPMethod::GET)([this]() {
 		// Recuperar todos los usuarios de la colección:
-
+		return "ok";
 	});
 
 	CROW_ROUTE(app, "/usuarios/<int>").methods(crow::HTTPMethod::GET)([this](int id) {
 		// Recuperar un usuario de la colección:
+		std::lock_guard<std::mutex> lock(this->mtx);
 
+		// Comprobar si existe el usuario;
+		if (this->usuarios.count(id) == 0) {
+			// Recurso no encontrado!
+			return crow::response(404, "Usuario id = " + std::to_string(id) + " no existe");
+		}
+		crow::json::wvalue resp;
+		resp["id"] = id;
+		resp["nombre"] = usuarios[id]["nombre"].s();
+		resp["edad"] = usuarios[id]["edad"].i();
+
+		return crow::response(resp);
 	});
 
 	CROW_ROUTE(app, "/usuarios/<int>").methods(crow::HTTPMethod::Delete)([this](int id) {
 		// Borrar un usuario de la colección:
-
+		return "ok";
 	});
 
 	CROW_ROUTE(app, "/usuarios/<int>").methods(crow::HTTPMethod::PUT)([this](const crow::request& req, int id) {
 		// Recibe un user, calcula el sig indice y lo coloca en la coleccion
-
+		return "ok";
 	});
 
 	app.port(8080).multithreaded().concurrency(std::thread::hardware_concurrency());
