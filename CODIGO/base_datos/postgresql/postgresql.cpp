f950e8608acd15a6595e6b5a228afd2cc869f102
aldebarran22 <56862985+aldebarran22@users.noreply.github.com>
2026-09-16 14:33:56 +0200

    87

---
+++ CODIGO/base_datos/postgresql/postgresql.cpp
@@ -0,0 +1,22 @@
+// postgresql.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
+//
+
+#include <libpq-fe.h>
+#include <iostream>
+
+void testConexion() {
+	try {
+		const char* conninfo = "host=127.0.0.1 port=5433 dbname=empresa3 user=antonio password=antonio";
+		PGconn* conn = PQconnectdb(conninfo);
+		std::cout << "Conexion ok" << std::endl;
+	}
+	catch (const std::exception& e) {
+		std::cout << "Error: " << e.what() << std::endl;
+	}
+}
+
+int main()
+{
+	testConexion();
+}
+
