28ed3be9a6313cd12fbcdf47d656e7880c440a14
aldebarran22 <56862985+aldebarran22@users.noreply.github.com>
2026-09-16 14:52:28 +0200

    12

--- CODIGO/base_datos/postgresql/postgresql.cpp
+++ CODIGO/base_datos/postgresql/postgresql.cpp
@@ -14,7 +14,31 @@ void testConexion() {
 		}
 		else {
 			std::cout << "Conexion ok" << std::endl;
+
+			// Listar los empleados:
+			PGresult* res = PQexec(conn, "select id, nombre, cargo from empleados");
+
+			if (PQresultStatus(res) != PGRES_TUPLES_OK) {
+				std::string mensaje = PQresultErrorMessage(res);								
+			}
+			else {
+				// Imprimir los resultados:
+				int n = PQntuples(res);
+				for (int i = 0; i < n; i++) {
+					std::cout <<
+						PQgetvalue(res, i, 0) <<
+						PQgetvalue(res, i, 1) <<
+						PQgetvalue(res, i, 2) << std::endl;
+				}
+
+			}
+
+			PQclear(res);
+
 		}
+
+		// Liberar la conexion:
+		PQfinish(conn);
 	}
 	catch (const std::exception& e) {
 		std::cout << "Error: " << e.what() << std::endl;
