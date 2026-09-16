a3d1d1f2c1ab35d401b44dcc0c86ea522cf31835
aldebarran22 <56862985+aldebarran22@users.noreply.github.com>
2026-09-16 13:59:09 +0200

    12

--- CODIGO/crow_json/crow_empleados/main.cpp
+++ CODIGO/crow_json/crow_empleados/main.cpp
@@ -10,6 +10,7 @@
 #include "EmpleadoRepositorio.h"
 #include "EmpleadoCache.h"
 #include "EmpleadoService.h"
+#include "ServicioCrow.h"
 
 
 void testConexion() {
@@ -116,7 +117,19 @@ void testFinal() {
 }
 
 void testMicroServicio() {
+    try {
+        soci::session sql(soci::mysql, "db=empresa3 user=antonio password=antonio host=127.0.0.1 port=3307");
 
+        EmpleadoRepositorio repo(sql);
+        EmpleadoCache cache;
+        EmpleadoService service(cache, repo);
+
+        ServicioCrow servicio(service);
+        servicio.run();
+    }
+    catch (const std::exception& e) {
+        std::cerr << "Error: " << e.what() << std::endl;
+    }
 }
 
 
@@ -125,5 +138,7 @@ int main()
     //testConexion();
     //testRepositorio();
     //testFinal();
+
+    testMicroServicio();
 }
 
