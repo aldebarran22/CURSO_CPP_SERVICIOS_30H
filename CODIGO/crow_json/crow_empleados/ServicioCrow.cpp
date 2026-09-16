667a16726c2bface8f1f183210cb76a52a76d676
aldebarran22 <56862985+aldebarran22@users.noreply.github.com>
2026-09-16 13:39:01 +0200

    87

--- CODIGO/crow_json/crow_empleados/ServicioCrow.cpp
+++ CODIGO/crow_json/crow_empleados/ServicioCrow.cpp
@@ -1,4 +1,13 @@
+#include <crow.h>
+#include <optional>
+#include <thread>
+#include <vector>
+#include <string>
+#include <nlohmann/json.hpp>
+
 #include "ServicioCrow.h"
+
+using json = nlohmann::json;
 
 ServicioCrow::ServicioCrow(EmpleadoService& service):service(service)
 {
@@ -6,7 +15,24 @@ ServicioCrow::ServicioCrow(EmpleadoService& service):service(service)
 
 void ServicioCrow::run()
 {
+	crow::SimpleApp app;
 
+	CROW_ROUTE(app, "/empleados").methods(crow::HTTPMethod::POST)([this](const crow::request& req) {});
+
+	CROW_ROUTE(app, "/empleados/<int>").methods(crow::HTTPMethod::GET)([this](int id) {
+	
+		auto empleado = this->service.read(id);
+		if (empleado) {
+			json doc = empleado;
+			return crow::response(doc.dump());
+
+		}
+		else {
+			return crow::response(404, std::string("El empleado: " + std::to_string(id) + " no existe"));
+		}
+	});
+
+	app.port(8080).multithreaded().concurrency(std::thread::hardware_concurrency()).run();
 }
 
 ServicioCrow::~ServicioCrow()
