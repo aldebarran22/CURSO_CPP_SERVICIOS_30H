3d874e0c43a9020500ecd95e1ace73a1ff52e434
aldebarran22 <56862985+aldebarran22@users.noreply.github.com>
2026-09-16 12:12:08 +0200

    87

---
+++ CODIGO/base_datos/redis/redis.cpp
@@ -0,0 +1,11 @@
+// redis.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
+//
+
+#include <iostream>
+#include <hiredis/hiredis.h>
+
+int main()
+{
+    
+}
+
