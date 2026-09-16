847b52cd256e89f491bb1bcbda33bfd3d81153d5
aldebarran22 <56862985+aldebarran22@users.noreply.github.com>
2026-09-16 12:25:03 +0200

    87

--- CODIGO/base_datos/redis/redis.cpp
+++ CODIGO/base_datos/redis/redis.cpp
@@ -4,8 +4,30 @@
 #include <iostream>
 #include <hiredis/hiredis.h>
 
+
+void testRedis() {
+    redisContext* context = redisConnect("127.0.0.1", 6379);
+    if (context == nullptr || context->err) {
+
+        if (context) {
+            std::cout << context->errstr << std::endl;
+        }
+        else {
+            std::cout << "No se ha podido crear el contexto";
+        }
+
+        return;
+    }
+
+    redisReply* reply = (redisReply*)redisCommand(context, "GET k1");
+    std::cout << reply->str << std::endl;
+    freeReplyObject(reply);
+
+    redisFree(context);
+}
+
 int main()
 {
-    
+    testRedis();
 }
 
