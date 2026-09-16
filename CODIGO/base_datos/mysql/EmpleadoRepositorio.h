c664ca6c2a026364dd0ae212c1e18a2ec241e59b
aldebarran22 <56862985+aldebarran22@users.noreply.github.com>
2026-09-16 11:38:28 +0200

    87

--- CODIGO/base_datos/mysql/EmpleadoRepositorio.h
+++ CODIGO/base_datos/mysql/EmpleadoRepositorio.h
@@ -1,5 +1,23 @@
 #pragma once
+
+#include <soci/soci.h>
+#include <vector>
+#include <optional>
+
+#include "Empleado.h"
+
 class EmpleadoRepositorio
 {
+private:
+	soci::session& sql;
+
+public:
+	EmpleadoRepositorio(soci::session& sql);
+	std::optional<Empleado> read(int id);
+	bool create(const Empleado& emp);
+	bool _delete(int id);
+	bool update(const Empleado& emp);
+	std::vector<Empleado> select();
+	~EmpleadoRepositorio();
 };
 
