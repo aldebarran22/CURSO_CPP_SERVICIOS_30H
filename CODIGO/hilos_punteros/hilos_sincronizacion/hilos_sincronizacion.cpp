c8d8945e0bf7c1a70e690d68a73baa73017029eb
aldebarran22 <56862985+aldebarran22@users.noreply.github.com>
2026-09-17 14:04:39 +0200

    87

--- CODIGO/hilos_punteros/hilos_sincronizacion/hilos_sincronizacion.cpp
+++ CODIGO/hilos_punteros/hilos_sincronizacion/hilos_sincronizacion.cpp
@@ -23,15 +23,37 @@ void resta() {
 	}
 }
 
+void suma2(std::mutex& mtx) {
+	for (int i = 0; i < IT; i++) {
+		mtx.lock();
+		contador_mutex++;
+		mtx.unlock();
+	}
+}
+
+void resta2(std::mutex& mtx) {
+	for (int i = 0; i < IT; i++) {
+		std::lock_guard m{ mtx };
+		contador_mutex--;
+	}
+}
+
 int main()
 {
 	std::mutex mtx;
-	std::thread h1{ suma };
+
+	std::thread h1{ suma };	
 	std::thread h2{ resta };
     
+	std::thread h3{ suma2, std::ref(mtx)};
+	std::thread h4{ resta2, std::ref(mtx)};
+
 	h1.join();
 	h2.join();
+	h3.join();
+	h4.join();
 
 	std::cout << "contador:" << contador << std::endl;
+	std::cout << "contador_mutex:" << contador_mutex << std::endl;
 }
 
