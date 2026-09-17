511e7583890a839efce49adf00ace30611f8b136
aldebarran22 <56862985+aldebarran22@users.noreply.github.com>
2026-09-17 14:23:00 +0200

    87

--- CODIGO/hilos_punteros/hilos_sincronizacion/hilos_sincronizacion.cpp
+++ CODIGO/hilos_punteros/hilos_sincronizacion/hilos_sincronizacion.cpp
@@ -4,6 +4,7 @@
 #include <iostream>
 #include <mutex>
 #include <thread>
+#include <vector>
 
 #define IT 1000000
 
@@ -12,12 +13,16 @@ int contador_mutex = 0;
 
 
 void suma() {
+	std::cout << "Id del this_thread: " << std::this_thread::get_id() << std::endl;
+	
 	for (int i = 0; i < IT; i++) {
 		contador++;
 	}
 }
 
 void resta() {
+	std::cout << "Id del this_thread: " << std::this_thread::get_id() << std::endl;
+	
 	for (int i = 0; i < IT; i++) {
 		contador--;
 	}
@@ -38,22 +43,72 @@ void resta2(std::mutex& mtx) {
 	}
 }
 
-int main()
-{
+void testMutex() {
 	std::mutex mtx;
 
-	std::thread h1{ suma };	
+	std::thread h1{ suma };
 	std::thread h2{ resta };
-    
-	std::thread h3{ suma2, std::ref(mtx)};
-	std::thread h4{ resta2, std::ref(mtx)};
+
+	std::thread h3{ suma2, std::ref(mtx) };
+	std::thread h4{ resta2, std::ref(mtx) };
 
 	h1.join();
 	h2.join();
 	h3.join();
 	h4.join();
 
+	std::cout << "Id del this_thread: " << std::this_thread::get_id() << std::endl;
+
 	std::cout << "contador:" << contador << std::endl;
 	std::cout << "contador_mutex:" << contador_mutex << std::endl;
+}
+
+class Hilo {
+	int id;
+	int n;
+	int tiempo;
+	std::mutex& m;
+
+public:
+	Hilo(int id, std::mutex& m, int n = 5, int tiempo = 1) :id(id), m(m), n(n), tiempo(tiempo) {}
+
+	void operator()() {
+
+		{
+			std::lock_guard<std::mutex> lock(m);
+			std::cout << "Iniciar el operador(" << id << ") con getId: " << std::this_thread::get_id() << std::endl;
+		}
+
+		for (int i = 0; i < n; i++) {
+			std::lock_guard<std::mutex> lock(m);
+			std::cout << "Operator(" << id << ") " << i << std::endl;
+			std::this_thread::sleep_for(std::chrono::seconds(tiempo));
+		}
+
+		{
+			std::lock_guard<std::mutex> lock(m);
+			std::cout << "Termina el operador(" << id << ")" << std::endl;
+		}
+	}
+};
+
+void testCrearHilos() {
+	std::mutex m;
+	std::vector<std::thread> hilos;
+
+	for (int i = 0; i < 3; i++) {
+		std::thread hilo(Hilo(i, std::ref(m)));
+		hilos.push_back(std::move(hilo));
+	}
+
+	for (auto& h : hilos) {
+		h.join();
+	}
+}
+
+int main()
+{
+	//testMutex();
+	testCrearHilos();
 }
 
