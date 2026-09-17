26920cca8eb1728d512c95cf06787c2d2ab1aba1
aldebarran22 <56862985+aldebarran22@users.noreply.github.com>
2026-09-17 09:23:10 +0200

    87

--- CODIGO/boost_web_sockets/ws_server/ws_server.cpp
+++ CODIGO/boost_web_sockets/ws_server/ws_server.cpp
@@ -8,10 +8,27 @@
 #include <boost/beast/websocket.hpp>
 #include <boost/asio.hpp>
 
+namespace beast = boost::beast;
+namespace websocket = beast::websocket;
+namespace net = boost::asio;
+namespace http = boost::beast::http;
 
+using tcp = net::ip::tcp;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    // Definir el contexto de E/S:
+    net::io_context ioc;
+
+    // Definir el endpoint en el server:
+    tcp::acceptor acceptor(ioc, tcp::endpoint(tcp::v4(), 80));
+
+    // Numero maximo de hilos del procesador:
+    unsigned int numHilos = std::thread::harware_concurrency();
+
+    // Construir el pool de hilos:
+    boost::asio::thread_pool pool(numHilos);
+
+
 }
 
