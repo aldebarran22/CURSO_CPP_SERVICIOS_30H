a84293d6a4189cc890a21bcd3d9ed9e47f3782f6
aldebarran22 <56862985+aldebarran22@users.noreply.github.com>
2026-09-17 13:03:34 +0200

    87

---
+++ CODIGO/crow_json/crow_json_cliente/crow_json_cliente.cpp
@@ -0,0 +1,12 @@
+// crow_json_cliente.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
+//
+
+#include <iostream>
+#include <string>
+#include <nlohmann/json.hpp>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
