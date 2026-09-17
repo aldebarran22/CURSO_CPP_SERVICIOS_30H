fe0da3c432bf0a224a43da9f7eae8ce306d18415
aldebarran22 <56862985+aldebarran22@users.noreply.github.com>
2026-09-17 10:50:04 +0200

    87

--- CODIGO/boost_web_sockets/wss_client/wss_client.cpp
+++ CODIGO/boost_web_sockets/wss_client/wss_client.cpp
@@ -7,11 +7,15 @@
 #include <boost/beast/core.hpp>
 #include <boost/beast/websocket.hpp>
 #include <boost/asio.hpp>
+#include <boost/beast/ssl.hpp>
+
+#define PORT 443
 
 namespace beast = boost::beast;
 namespace websocket = beast::websocket;
 namespace net = boost::asio;
 namespace http = boost::beast::http;
+namespace ssl = net::ssl;
 
 using tcp = net::ip::tcp;
 
@@ -22,18 +26,29 @@ int main()
 		// Definir el contexto de E/S:
 		net::io_context ioc;
 
+		// Definir el contexto de seguridad para el cliente:
+		ssl::context ctx(ssl::context::tlsv12_client);
+
+		// Si no tenemos una CA (Autoridad de certificacion):
+		ctx.set_verify_mode(ssl::verify_none); // No verifica la CA:
+
+		// Si tenemos una CA  (Autoridad de certificacion):
+		//ctx.set_verify_mode(ssl::verify_peer);
+		//ctx.load_verify_file("cap.pem");
+
 		// Para localizar el endpoint en el servidor:
 		tcp::resolver resolver(ioc);
 
-		// Definir el websocket para el cliente:
-		websocket::stream<tcp::socket> ws(ioc);
+		// Definir el websocket SSL para el cliente:
+		websocket::stream<beast::ssl_stream<tcp::socket>> ws(ioc, ctx);
+
 
 		// Localizar el servidor:
-		auto const results = resolver.resolve("localhost", "80");
+		auto const results = resolver.resolve("localhost", std::to_string(PORT);
 
 		// Intentar conectar con el endpoint del servidor:
-		net::connect(ws.next_layer(), results.begin(), results.end());
-
+		net::connect(ws.next_layer().next_layer(), results.begin(), results.end());
+		ws.next_layer().handshake(ssl::stream_base::client);
 		ws.handshake("localhost", "/");
 
 		// Enviar y recibir informacion con el servidor:
