4ea15f07a67c481421387b62aecf1ed354662115
aldebarran22 <56862985+aldebarran22@users.noreply.github.com>
2026-09-17 12:21:58 +0200

    87

--- CODIGO/crow_json/crow_jwt/crow_jwt.cpp
+++ CODIGO/crow_json/crow_jwt/crow_jwt.cpp
@@ -2,9 +2,38 @@
 //
 
 #include <iostream>
+#include <crow.h>
+#include <string>
+#include <nlohmann/json.hpp>
+#include <jwt-cpp/jwt.h>
+
+// Creadenciales del usuarios:
+#define USER "admin"
+#define PASS "1234"
+
+
+using json = nlohmann::json;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	crow::SimpleApp app;
+
+	CROW_ROUTE(app, "/login").methods(crow::HTTPMethod::POST)([](const crow::request& req) {
+
+		try {
+			// Parsear el json:
+			json j = json::parse(req.body);
+
+			// Validacion de las etiquetas en el json:
+			if (!j.contains("user") || !j.contains("pass")) {
+				return crow::response(400, "Json incorrecto. Faltan etiquetas");
+			}
+
+
+		}
+		catch (const std::exception& e) {
+			return crow::response(500, std::string(e.what()));
+		}
+	});
 }
 
