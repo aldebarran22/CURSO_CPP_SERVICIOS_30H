a641e28a2000a929d692d9c69fee4f45863a00b1
aldebarran22 <56862985+aldebarran22@users.noreply.github.com>
2026-09-17 09:17:43 +0200

    87

---
+++ CODIGO/boost_web_sockets/ws_server/ws_server.cpp
@@ -0,0 +1,17 @@
+// ws_server.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
+//
+
+#include <iostream>
+#include <thread>
+
+#include <boost/beast/core.hpp>
+#include <boost/beast/websocket.hpp>
+#include <boost/asio.hpp>
+
+
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
