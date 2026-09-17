035a6fab2470064c77713064c6dc20dce5f889a6
aldebarran22 <56862985+aldebarran22@users.noreply.github.com>
2026-09-17 13:14:49 +0200

    87

--- CODIGO/crow_json/crow_json_cliente/crow_json_cliente.cpp
+++ CODIGO/crow_json/crow_json_cliente/crow_json_cliente.cpp
@@ -16,7 +16,7 @@ using json = nlohmann::json;
 int main()
 {
 	try {
-		json j = { {"user", USER}, {"pass", PASS} };
+		json j = { {"user", USER}, {"pass", PASS}};
 
 		auto response = cpr::Post(
 			cpr::Url{ "http://localhost:8080/login" },
@@ -25,6 +25,16 @@ int main()
 		);
 
 		std::cout << "Respuesta de login: " << response.text << std::endl;
+		json resp = json::parse(response.text);
+		std::string token = resp.at("token");
+		std::cout << "\n\nToken: " << token << std::endl;
+
+		auto response2 = cpr::Get(
+			cpr::Url{ "http://localhost:8080/app" },
+			cpr::Header{ {"Accept", "application/json"} }
+		);
+
+		std::cout << "Respuesta de app: " << response2.text << std::endl;
 	}
 	catch (const std::exception& e) {
 		std::cerr << "ERROR: " << e.what() << std::endl;
