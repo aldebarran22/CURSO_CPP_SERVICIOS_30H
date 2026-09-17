b6962c29f3021fd50dc92b940ae5a646ae74588e
aldebarran22 <56862985+aldebarran22@users.noreply.github.com>
2026-09-17 10:33:15 +0200

    87

--- CODIGO/boost_web_sockets/wss_server/wss_server.cpp
+++ CODIGO/boost_web_sockets/wss_server/wss_server.cpp
@@ -21,6 +21,38 @@ using tcp = net::ip::tcp;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    // Definir el contexto de E/S:
+    net::io_context ioc;
+
+    // Definir el contexto SSL:
+    ssl::context ctx(ssl::context::tslv12_server);
+
+    // Indicar cuales son los ficheros de los certificados:
+    ctx.use_certificate_chain_file("..\\certificados\\cert.pem");
+    ctx.use_private_key_file("..\\certificados\\key.pem", ssl::context::file_format::pem);
+
+    // Definir el endpoint en el server:
+    tcp::acceptor acceptor(ioc, tcp::endpoint(tcp::v4(), PORT));
+
+    // Numero maximo de hilos del procesador:
+    unsigned int numHilos = std::thread::hardware_concurrency();
+
+    // Construir el pool de hilos:
+    boost::asio::thread_pool pool(numHilos);
+
+    while (true) {
+        // Es un socket por cada conexion:
+        tcp::socket socket(ioc);
+
+        std::cout << "Servidor a la espera de clientes ..." << std::endl;
+
+        // Aceptar un nuevo cliente:
+        acceptor.accept(socket);
+
+        // Lanzar el hilo para atender al cliente que acaba de conectar:
+        boost::asio::post(pool, [s = std::move(socket), &ctx]() mutable {
+            do_session(std::move(s), ctx);
+        });
+    }
 }
 
