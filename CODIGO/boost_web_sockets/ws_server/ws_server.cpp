2ec4b6d465364d8d768c53014670c8eadbc68aaa
aldebarran22 <56862985+aldebarran22@users.noreply.github.com>
2026-09-17 09:39:19 +0200

    87

--- CODIGO/boost_web_sockets/ws_server/ws_server.cpp
+++ CODIGO/boost_web_sockets/ws_server/ws_server.cpp
@@ -15,6 +15,43 @@ namespace http = boost::beast::http;
 
 using tcp = net::ip::tcp;
 
+void do_session(tcp::socket socket)
+{
+    try {
+        // Definir el websocket: stream de tipo TCP:
+        websocket::stream<tcp::socket> ws(std::move(socket));
+        ws.accept();
+
+        for (;;) {
+            // Buffer de E/S:
+            beast::flat_buffer buffer;
+
+            ws.read(buffer);
+            if (!ws.binary()) {
+                std::cout << "El cliente envia texto: " << beast::buffers_to_string(buffer.data()) << std::endl;
+                ws.text(ws.got_text());
+
+                // Devolver al cliente el mismo mensaje:
+                ws.write(buffer.data());
+            }
+            else {
+                std::cout << "datos en binario" << std::endl;
+            }
+        }
+    }
+    catch (const beast::system_error& se) {
+        if (se.code() != websocket::error::closed) {
+            std::cerr << "Error en la sesion: " << se.what() << std::endl;
+        }
+        else {
+            std::cout << "Cliente desconectado ... " << std::endl;
+        }
+    }
+    catch (const std::exception& e) {
+        std::cerr << "Otro error : " << e.what() << std::endl;
+    }
+}
+
 int main()
 {
     // Definir el contexto de E/S:
@@ -24,7 +61,7 @@ int main()
     tcp::acceptor acceptor(ioc, tcp::endpoint(tcp::v4(), 80));
 
     // Numero maximo de hilos del procesador:
-    unsigned int numHilos = std::thread::harware_concurrency();
+    unsigned int numHilos = std::thread::hardware_concurrency();
 
     // Construir el pool de hilos:
     boost::asio::thread_pool pool(numHilos);
