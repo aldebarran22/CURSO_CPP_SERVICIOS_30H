2d43ffedd0b726c042e2c77cbabbbad979b135ea
aldebarran22 <56862985+aldebarran22@users.noreply.github.com>
2026-09-18 09:27:09 +0200

    87

--- CODIGO/hilos_punteros/punteros/punteros.cpp
+++ CODIGO/hilos_punteros/punteros/punteros.cpp
@@ -28,6 +28,8 @@ void testSmartPointers()
 {
     std::unique_ptr<std::unique_ptr<int[]>[]> ptr = std::make_unique<std::unique_ptr<int[]>[]>(10);
 
+    //auto ptr2 = std::move(ptr); Mover si se permite!
+
     // Crear las filas:
     for (int i = 0; i < 10; i++) {
         ptr[i] = std::make_unique<int[]>(10);
@@ -42,11 +44,30 @@ void testVector()
     matriz.at(3).at(5) = 0;
 }
 
+struct A {
+    A(int a) {
+        std::cout << "Se crea el objeto…\n"; }
+            
+    ~A() {
+            std::cout << "Se elimina el objeto…\n"; }
+};
 
 int main()
 {
     //testPunteros();
     //testSmartPointers();
-    testVector();
+    //testVector();
+
+    // Equivalente a: std:: shared_ptr<A>(new A(10));
+    auto a_ptr = std::make_shared<A >(10);
+
+    std::cout << a_ptr.use_count() << std::endl;
+    {
+        auto b_ptr = a_ptr;
+        std::cout << a_ptr.use_count() << std::endl;
+    }
+    std::cout << "En el scope de main\n";
+    std::cout << a_ptr.use_count() << std::endl;
+    return 0;
 }
 
