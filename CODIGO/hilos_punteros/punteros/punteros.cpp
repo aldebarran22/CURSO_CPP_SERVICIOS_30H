f94d1da9ffdfa88a6b812adfba1b9869d2be4076
aldebarran22 <56862985+aldebarran22@users.noreply.github.com>
2026-09-18 09:14:13 +0200

    87

--- CODIGO/hilos_punteros/punteros/punteros.cpp
+++ CODIGO/hilos_punteros/punteros/punteros.cpp
@@ -13,6 +13,7 @@ void testPunteros()
         ptr[i] = new int[10];
     }
 
+    
     // Se libera la memoria al reves:
     for (int i = 0; i < 10; i++) {
         delete[] ptr[i];
@@ -23,7 +24,12 @@ void testPunteros()
 
 void testSmartPointers()
 {
+    std::unique_ptr<std::unique_ptr<int[]>[]> ptr = std::make_unique<std::unique_ptr<int[]>[]>(10);
 
+    // Crear las filas:
+    for (int i = 0; i < 10; i++) {
+        ptr[i] = std::make_unique<int[]>(10);
+    }
 }
 
 
